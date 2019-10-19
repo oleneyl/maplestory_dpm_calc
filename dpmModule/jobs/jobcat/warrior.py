d40ae9ed1fe824012b14857f6bffef54823932ef
cheese98 <cheesej98@gmail.com>
2019-10-19 10:32:37 +0000

    오라웨폰 추가

--- dpmModule/jobs/jobcat/warrior.py
+++ dpmModule/jobs/jobcat/warrior.py
@@ -4,6 +4,10 @@ from ...kernel.core import CharacterModifier as MDF
 from ...character import characterKernel as ck
 from functools import partial
 
+def AuraWeaponWrapper(vEhc, num1, num2):
+    AuraWeapon = core.SummonSkill("오라웨폰", 0, 6000, (500 + 20 * vEhc.getV(num1, num2)), 6, (80 +2*vEhc.getV(num1, num2)) * 1000, cooltime = 180 * 1000, modifier = core.CharacterModifier(armor_ignore = 15, pdamage_indep = (vEhc.getV(num1, num2) // 5))).isV(vEhc, num1, num2).wrap(core.SummonSkillWrapper)
+    return AuraWeapon
+	
 '''class FridWrapper(core.BuffSkillWrapper):
 	# num1, num2
     def __init__(self, vEhc, num1, num2):
--- execute2.py
+++ execute2.py
@@ -1,6 +1,6 @@
 import dpmModule
 from dpmModule.util.dpmgenerator import IndividualDPMGenerator
 import dpmModule.character.characterTemplateHigh as template
-job_name = '메르세데스'
+job_name = '히어로'
 gen = IndividualDPMGenerator(job_name, template.getU6000CharacterTemplate)
 print(job_name, gen.get_dpm(ulevel = 6000))
