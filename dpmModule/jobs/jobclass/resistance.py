e6f392f790e4df1f530aaeabe7ce5932a158a26d
cheese98 <cheesej98@gmail.com>
2019-11-08 17:11:03 +0900

    메용2 레지 추가

--- dpmModule/jobs/jobclass/resistance.py
+++ dpmModule/jobs/jobclass/resistance.py
@@ -3,8 +3,12 @@ from ...kernel.core import VSkillModifier as V
 from ...kernel.core import CharacterModifier as MDF
 from ...character import characterKernel as ck
 from functools import partial
+import adventurer
 
 def ResistanceLineInfantryWrapper(vEhc, num1, num2):
     ResistanceLineInfantry = core.SummonSkill("레지스탕스 라인 인팬트리", 360, 1000, 215+8*vEhc.getV(num1, num2), 9, 10*1000, cooltime = 25000).isV(vEhc,num1, num2).wrap(core.SummonSkillWrapper)
     return ResistanceLineInfantry
 
+# 메용2는 모험가 파일에 맡김. 더 나은 방법 있으면 수정 필요.
+def MapleHeroes2Wrapper(vEhc, num1, num2):
+    adventurer.MapleHeroes2Wrapper(vEhc, num1, num2)
