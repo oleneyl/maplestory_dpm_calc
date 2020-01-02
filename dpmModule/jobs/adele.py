750cda966e6b925891953d7e5d0b57df7d27b53c
cheese98 <cheesej98@gmail.com>
2020-01-02 22:01:42 +0900

    added base on adele.py

---
+++ dpmModule/jobs/adele.py
@@ -0,0 +1,18 @@
+from ..kernel import core
+from ..kernel.core import VSkillModifier as V
+from ..character import characterKernel as ck
+from functools import partial
+from ..status.ability import Ability_tool
+from ..execution.rules import RuleSet, ConcurrentRunRule, InactiveRule
+from . import globalSkill
+from .jobclass import cygnus
+from .jobbranch import flora
+
+class JobGenerator(ck.JobGenerator):
+    def __init__(self):
+        super(JobGenerator, self).__init__()
+        self.buffrem = False
+        self.vEnhanceNum = 10
+        self.jobtype = "str"
+        self.ability_list = Ability_tool.get_ability_set('boss_pdamage', 'crit', 'buff_rem')
+        self.preEmptiveSkills = 1
