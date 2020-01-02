819cf5b6d5026aa2a76950517cc43437a05e82e3
cheese98 <cheesej98@gmail.com>
2020-01-02 22:09:19 +0900

    added explanation for full_test.py

--- full_test.py
+++ full_test.py
@@ -4,9 +4,15 @@ from dpmModule.util.dpmgenerator import IndividualDPMGenerator
 import dpmModule.character.characterTemplateHigh as template
 import dpmModule.jobs as maplejobs
 
+'''
+full_test.py
+전체 테스트를 위한 스크립트입니다.
+
+등록된 모든 직업의 표준 DPM 수치를 계산하여 파일로 출력합니다.
+'''
+
 sys.stdout = open('output.txt','w')
 
-dpm_list = []
 for value in maplejobs.jobList.values():
     gen = IndividualDPMGenerator(value, template.getU6000CharacterTemplate)
     print(value, gen.get_dpm(ulevel = 6000))
