bbdc98eac6957b6079ac1e926198da9e9457bcd5
cheese98 <cheesej98@gmail.com>
2020-02-22 23:13:09 +0900

    시그너스의 축복 정확한 값으로 수정

--- dpmModule/jobs/jobclass/cygnus.py
+++ dpmModule/jobs/jobclass/cygnus.py
@@ -15,10 +15,19 @@ class CygnusBlessWrapper(core.BuffSkillWrapper):
         self.skill_importance = skill_importance
         self.enhance_importance = enhance_importance
         
-        # 스킬 딜레이, 쿨타임 확인 필요
-        skill = core.BuffSkill("초월자 시그너스의 축복" if isUpgraded else "여제 시그너스의 축복" , 450, 45000, cooltime = 240*1000).wrap(core.BuffSkillWrapper)
+        skill = core.BuffSkill("초월자 시그너스의 축복" if isUpgraded else "여제 시그너스의 축복" , 630, 45000, cooltime = 240*1000).wrap(core.BuffSkillWrapper)
         super(CygnusBlessWrapper, self).__init__(skill)
 
+        self.damage_point = 4 + (enhancer.getV(self.skill_importance, self.enhance_importance))//15
+        self.damage_limit = 90
+        #interval: 데미지 증가 주기 (모름)
+        self.interval = 4
+        
+        # 업그레이드 상태일경우 해당 스펙 적용
+        if self.isUpgraded:
+            self.damage_point += 2
+            self.damage_limit += 30
+            
         self.passedTime = 0
         self.serverlag = serverlag
 
@@ -26,22 +35,11 @@ class CygnusBlessWrapper(core.BuffSkillWrapper):
         if self.onoff:
             self.passedTime += time
         super(CygnusBlessWrapper, self).spend_time(time)
-    
-    #세부값 확인해서 변경필요
-    #MP 500 소비, 45초 동안 데미지 25% 증가, 일정 시간마다 HP 4%씩 회복 및 데미지 3% 추가 증가, 시그너스의 축복으로 증가하는 데미지는 합 적용이며 최대 90%까지 증가
-    #MP 500 소비, 45초 동안 데미지 25% 증가 및 최대 HP의 일정 비율로 피해를 입히는 공격을 포함한 피격 데미지 5% 감소, 일정 시간마다 HP 7%씩 회복 및 데미지 5% 추가 증가, 시그너스의 축복으로 증가하는 데미지는 합 적용이며 최대 120%까지 증가
-
-    def calc_damage(self):
-        interval = 4
-        if self.isUpgraded:
-            pd_value = min(25 + 5 * (self.passedTime // ((interval + self.serverlag) * 1000)), 120)
-        else:
-            pd_value = min(25 + 3 * (self.passedTime // ((interval + self.serverlag) * 1000)), 90)
-        return pd_value
 
     def get_modifier(self):
+        
         if self.onoff:
-            return core.CharacterModifier(pdamage = self.calc_damage())
+            return core.CharacterModifier(pdamage = min(enhancer.getV(self.skill_importance, self.enhance_importance) + self.damage_point * (self.passedTime // ((self.interval + self.serverlag) * 1000)), self.damage_limit))
         else:
             return core.CharacterModifier()
         
