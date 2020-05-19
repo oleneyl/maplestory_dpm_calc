84da5c89413ec62be7d5f4e180a72a7df9489150
oleneyl <39217532+oleneyl@users.noreply.github.com>
2020-05-19 15:55:30 +0900

    Add restriction option

--- dpmModule/util/dpmgenerator.py
+++ dpmModule/util/dpmgenerator.py
@@ -26,7 +26,7 @@ class IndividualDPMGenerator():
     def set_runtime(self, time):
         self.runtime = time
 
-    def get_dpm(self, ulevel = 6000, weaponstat = [4,9], printFlag = False):
+    def get_dpm(self, ulevel = 6000, weaponstat = [4,9], printFlag = False, restricted = True):
         #TODO target을 동적으로 생성할 수 있도록.
         target = self.template(maplejobs.weaponList[self.job])
         gen = (self.supplier).JobGenerator()
@@ -41,10 +41,12 @@ class IndividualDPMGenerator():
             ]), 
             [rules.UniquenessRule()]) #가져온 그래프를 토대로 스케줄러를 생성합니다.
         analytics = core.Analytics(printFlag=printFlag)  #데이터를 분석할 분석기를 생성합니다.
+        if printFlag:
+            print(target.get_modifier())
         control = core.Simulator(sche, target, analytics) #시뮬레이터에 스케줄러, 캐릭터, 애널리틱을 연결하고 생성합니다.
         control.start_simulation(self.runtime)
         control.analytics.statistics()
-        return control.getDPM()
+        return control.getDPM(restricted=restricted)
 
     def get_detailed_dpm(self, ulevel = 6000, weaponstat = [4,9]):
         #TODO target을 동적으로 생성할 수 있도록.
