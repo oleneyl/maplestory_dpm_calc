2953e46560969c17cd2a883a4dd0f8c6ff54740d
cheese98 <cheesej98@gmail.com>
2020-06-03 19:13:39 +0900

    공용 스킬 패시브 추가 및 세부값 수정

--- dpmModule/jobs/globalSkill.py
+++ dpmModule/jobs/globalSkill.py
@@ -5,32 +5,48 @@ from ..kernel import core
 
 # 데벤져 추가할시 쓸만한 하이퍼바디 추가바람
 
-def soul_contract():
-    SoulContract = core.BuffSkill("소울 컨트랙트", 600, 10*1000, cooltime = 90*1000, pdamage = 45, rem = True).wrap(core.BuffSkillWrapper)
+def useful_hyper_body_demonavenger(slevel = 1):
+    # 딜레이 클라기준
+    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디 (데몬어벤져)", 600, (180 + slevel * 3)*1000, pstat_main = 40, stat_sub = 1, rem = False).wrap(core.BuffSkillWrapper)
+    return UsefulHyperBody
+
+def useful_hyper_body_zenon(slevel = 1):
+    # 마나뻥 처리는 직업코드에서
+    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디", 600, (180 + slevel * 3)*1000, stat_main = 1, stat_sub = 1, rem = False).wrap(core.BuffSkillWrapper)
+    return UsefulHyperBody
+
+# 기본 90초, 직업별 딜사이클에 따라 쿨타임 조절가능
+def soul_contract(Cool = 90*1000):
+    SoulContract = core.BuffSkill("소울 컨트랙트", 900, 10*1000, cooltime = Cool, pdamage = 45, rem = True).wrap(core.BuffSkillWrapper)
     return SoulContract
 
-def maple_heros(level):
-    MapleHeros = core.BuffSkill("메이플 용사", 0, 900*1000, stat_main = 0.15*(25 + level * 5), rem = True).wrap(core.BuffSkillWrapper)
+# 쓸컴뱃 = 1, 팔라딘 = 2
+def maple_heros(level, combat = 0):
+    MapleHeros = core.BuffSkill("메이플 용사", 0, (900+15*combat)*1000, stat_main = (0.15 + 0.005 * combat)*(25 + level * 5), rem = True).wrap(core.BuffSkillWrapper)
     return MapleHeros
 
 def useful_combat_orders():
-    UsefulCombatOrders = core.BuffSkill("쓸만한 컴뱃 오더스", 1350, 183*1000, rem = False).wrap(core.BuffSkillWrapper)
+    UsefulCombatOrders = core.BuffSkill("쓸만한 컴뱃 오더스", 1500, (180 + slevel * 3)*1000, rem = False).wrap(core.BuffSkillWrapper)
     return UsefulCombatOrders
     
-def useful_sharp_eyes():
-    UsefulSharpEyes = core.BuffSkill("쓸만한 샤프 아이즈", 1080, 183 * 1000, rem = False, crit = 10, crit_damage = 8).wrap(core.BuffSkillWrapper)
+def useful_sharp_eyes(slevel = 1):
+    UsefulSharpEyes = core.BuffSkill("쓸만한 샤프 아이즈", 900, (180 + slevel * 3) * 1000, rem = False, crit = 10, crit_damage = 8, stat_main = (slevel + 4) // 5, stat_sub = (slevel + 4) // 5).wrap(core.BuffSkillWrapper)
     return UsefulSharpEyes
     
-def useful_wind_booster():
-    UsefulWindBooster = core.BuffSkill("쓸만한 윈드 부스터", 1080, 183*1000, rem = False).wrap(core.BuffSkillWrapper)
+def useful_wind_booster(slevel = 1):
+    UsefulWindBooster = core.BuffSkill("쓸만한 윈드 부스터", 900, (180 + slevel * 3) * 1000, rem = False, stat_main = (slevel + 4) // 5, stat_sub = (slevel + 4) // 5).wrap(core.BuffSkillWrapper)
     return UsefulWindBooster
+
+def useful_advanced_bless(slevel = 1):
+    # TODO: HP, MP 475 증가 반영
+    UsefulAdvancedBless = core.BuffSkill("쓸만한 어드밴스드 블레스", (180 + slevel * 3) * 1000, rem = False, att = 20).wrap(core.BuffSkillWrapper)
 
 def SpiderInMirrorWrapper(enhancer, skill_importance, enhance_importance, LEVEL):
     MirrorBreak = core.DamageSkill("스파이더 인 미러(공간 붕괴)", 960, 750+30*enhancer.getV(skill_importance, enhance_importance), 12, cooltime = 250*1000).wrap(core.DamageSkillWrapper)
     # 5번 연속 공격 후 종료, 재돌입 대기시간 3초
-    MirrorSpider = core.SummonSkill("스파이더 인 미러(거울 속의 거미)", 0, 3000, 175+17*enhancer.getV(skill_importance, enhance_importance), 8, 15*1000).wrap(core.SummonSkillWrapper)
+    MirrorSpider = core.SummonSkill("스파이더 인 미러(거울 속의 거미)", 0, 3000, 175+17*enhancer.getV(skill_importance, enhance_importance), 8, 15*1000, cooltime = -1).wrap(core.SummonSkillWrapper)
     MirrorBreak.onAfter(MirrorSpider)
     # 235레벨 이상만 사용가능
     SpiderInMirror = core.OptionalElement(lambda : (LEVEL >= 235), MirrorBreak)
 
-    return SpiderInMirror
+    return SpiderInMirror, MirrorBreak, MirrorSpider
