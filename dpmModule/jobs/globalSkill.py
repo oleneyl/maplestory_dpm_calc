e3501a443ff99b3e4b73189f5b70a5b92f892bba
cheese98 <cheesej98@gmail.com>
2020-06-04 11:34:39 +0900

    쓸스킬 레벨당 패시브 스탯 반영

--- dpmModule/jobs/globalSkill.py
+++ dpmModule/jobs/globalSkill.py
@@ -3,21 +3,23 @@ from ..kernel import core
 
 #_SoulContract = core.BuffSkill("SoulContract", 600, 1000, cooltime = 9000, pdamage = 45)
 
-# 데벤져 추가할시 쓸만한 하이퍼바디 추가바람
+# TODO: CharacterModifier 반환 방식으로 변경
+passiveStat = lambda slevel : (slevel + 4) // 5
 
+# 미완성 코드
 def useful_hyper_body_demonavenger(slevel = 1):
     # 딜레이 클라기준
-    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디 (데몬어벤져)", 600, (180 + slevel * 3)*1000, pstat_main = 40, stat_sub = 1, rem = False).wrap(core.BuffSkillWrapper)
+    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디 (데몬어벤져)", 600, (180 + slevel * 3)*1000, pstat_main = 40, stat_sub = passiveStat(slevel), rem = False).wrap(core.BuffSkillWrapper)
     return UsefulHyperBody
 
 def useful_hyper_body_zenon(slevel = 1):
     # 마나뻥 처리는 직업코드에서
-    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디", 600, (180 + slevel * 3)*1000, stat_main = 1, stat_sub = 1, rem = False).wrap(core.BuffSkillWrapper)
+    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디", 600, (180 + slevel * 3)*1000, stat_main = passiveStat(slevel), stat_sub = passiveStat(slevel), rem = False).wrap(core.BuffSkillWrapper)
     return UsefulHyperBody
 
 # 기본 90초, 직업별 딜사이클에 따라 쿨타임 조절가능
 def soul_contract(Cool = 90*1000):
-    SoulContract = core.BuffSkill("소울 컨트랙트", 900, 10*1000, cooltime = Cool, pdamage = 45, rem = True).wrap(core.BuffSkillWrapper)
+    SoulContract = core.BuffSkill("소울 컨트랙트", 900, 10*1000, cooltime = Cool, pdamage = 45, rem = True, red = True).wrap(core.BuffSkillWrapper)
     return SoulContract
 
 # 쓸컴뱃 = 1, 팔라딘 = 2
@@ -25,21 +27,22 @@ def maple_heros(level, combat = 0):
     MapleHeros = core.BuffSkill("메이플 용사", 0, (900+15*combat)*1000, stat_main = (0.15 + 0.005 * combat)*(25 + level * 5), rem = True).wrap(core.BuffSkillWrapper)
     return MapleHeros
 
+# 1레벨
 def useful_combat_orders():
-    UsefulCombatOrders = core.BuffSkill("쓸만한 컴뱃 오더스", 1500, (180 + slevel * 3)*1000, rem = False).wrap(core.BuffSkillWrapper)
+    UsefulCombatOrders = core.BuffSkill("쓸만한 컴뱃 오더스", 1500, (180 + 3)*1000, rem = False).wrap(core.BuffSkillWrapper)
     return UsefulCombatOrders
     
 def useful_sharp_eyes(slevel = 1):
-    UsefulSharpEyes = core.BuffSkill("쓸만한 샤프 아이즈", 900, (180 + slevel * 3) * 1000, rem = False, crit = 10, crit_damage = 8, stat_main = (slevel + 4) // 5, stat_sub = (slevel + 4) // 5).wrap(core.BuffSkillWrapper)
+    UsefulSharpEyes = core.BuffSkill("쓸만한 샤프 아이즈", 900, (180 + slevel * 3) * 1000, rem = False, crit = 10, crit_damage = 8, stat_main = passiveStat(slevel), stat_sub = passiveStat(slevel)).wrap(core.BuffSkillWrapper)
     return UsefulSharpEyes
     
 def useful_wind_booster(slevel = 1):
-    UsefulWindBooster = core.BuffSkill("쓸만한 윈드 부스터", 900, (180 + slevel * 3) * 1000, rem = False, stat_main = (slevel + 4) // 5, stat_sub = (slevel + 4) // 5).wrap(core.BuffSkillWrapper)
+    UsefulWindBooster = core.BuffSkill("쓸만한 윈드 부스터", 900, (180 + slevel * 3) * 1000, rem = False, stat_main = passiveStat(slevel), stat_sub = passiveStat(slevel)).wrap(core.BuffSkillWrapper)
     return UsefulWindBooster
 
-def useful_advanced_bless(slevel = 1):
+def useful_advanced_bless(slevel = 1, useHP = False):
     # TODO: HP, MP 475 증가 반영
-    UsefulAdvancedBless = core.BuffSkill("쓸만한 어드밴스드 블레스", (180 + slevel * 3) * 1000, rem = False, att = 20).wrap(core.BuffSkillWrapper)
+    UsefulAdvancedBless = core.BuffSkill("쓸만한 어드밴스드 블레스", (180 + slevel * 3) * 1000, rem = False, att = 20, stat_main = 475 * useHP).wrap(core.BuffSkillWrapper)
 
 def SpiderInMirrorWrapper(enhancer, skill_importance, enhance_importance, LEVEL):
     MirrorBreak = core.DamageSkill("스파이더 인 미러(공간 붕괴)", 960, 750+30*enhancer.getV(skill_importance, enhance_importance), 12, cooltime = 250*1000).wrap(core.DamageSkillWrapper)
