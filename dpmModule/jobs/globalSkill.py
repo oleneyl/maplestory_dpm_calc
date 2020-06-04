5a660db882809dec497d9de221ad2e5c0ef55634
cheese98 <cheesej98@gmail.com>
2020-06-04 12:01:14 +0900

    SpiderInMirror 수정

--- dpmModule/jobs/globalSkill.py
+++ dpmModule/jobs/globalSkill.py
@@ -44,12 +44,18 @@ def useful_advanced_bless(slevel = 1, useHP = False):
     # TODO: HP, MP 475 증가 반영
     UsefulAdvancedBless = core.BuffSkill("쓸만한 어드밴스드 블레스", (180 + slevel * 3) * 1000, rem = False, att = 20, stat_main = 475 * useHP).wrap(core.BuffSkillWrapper)
 
-def SpiderInMirrorWrapper(enhancer, skill_importance, enhance_importance, LEVEL):
-    MirrorBreak = core.DamageSkill("스파이더 인 미러(공간 붕괴)", 960, 750+30*enhancer.getV(skill_importance, enhance_importance), 12, cooltime = 250*1000).wrap(core.DamageSkillWrapper)
-    # 5번 연속 공격 후 종료, 재돌입 대기시간 3초
-    MirrorSpider = core.SummonSkill("스파이더 인 미러(거울 속의 거미)", 0, 3000, 175+17*enhancer.getV(skill_importance, enhance_importance), 8, 15*1000, cooltime = -1).wrap(core.SummonSkillWrapper)
-    MirrorBreak.onAfter(MirrorSpider)
-    # 235레벨 이상만 사용가능
-    SpiderInMirror = core.OptionalElement(lambda : (LEVEL >= 235), MirrorBreak)
+class SpiderInMirrorBuilder():
+    def __init__(self, enhancer, skill_importance, enhance_importance, chtr_level):
+        self.MirrorBreak = core.DamageSkill(
+            "스파이더 인 미러(공간 붕괴)", 960, 750+30*enhancer.getV(skill_importance, enhance_importance), 12, cooltime = 250*1000
+        ).wrap(core.DamageSkillWrapper)
+        # 5번 연속 공격 후 종료, 재돌입 대기시간 3초
+        self.MirrorSpider = core.SummonSkill(
+            "스파이더 인 미러(거울 속의 거미)", 0, 3000, 175+17*enhancer.getV(skill_importance, enhance_importance), 8, 15*1000, cooltime = -1
+        ).wrap(core.SummonSkillWrapper)
+        self.MirrorBreak.onAfter(self.MirrorSpider.controller(3000))
+        self.SpiderInMirror = core.OptionalElement(cthr_level >= 235, MirrorBreak)
 
-    return SpiderInMirror, MirrorBreak, MirrorSpider
+    def get_skill(self):
+        return self.SpiderInMirror
+
