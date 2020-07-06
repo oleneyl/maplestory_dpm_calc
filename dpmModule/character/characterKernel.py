5246fd9754c7364bacf6ad22c0b3b7a3941550d5
oleneyl <39217532+oleneyl@users.noreply.github.com>
2020-07-06 18:59:08 +0900

    enable ruleset

--- dpmModule/character/characterKernel.py
+++ dpmModule/character/characterKernel.py
@@ -5,6 +5,7 @@ from ..item.ItemKernel import Item
 from ..status.ability import Ability_tool, Ability_grade
 from ..kernel.graph import GlobalOperation, initialize_global_properties, _unsafe_access_global_storage
 from ..kernel import policy
+
 MDF = CharacterModifier
 '''Clas AbstractCharacter : Basic template for build specific User. User is such object that contains
 - Items
@@ -145,7 +146,7 @@ class JobGenerator():
         return
 
     def get_ruleset(self):
-        return []
+        return
 
     def get_predefined_rules(self, rule_type):
         ruleset = self.get_ruleset()
