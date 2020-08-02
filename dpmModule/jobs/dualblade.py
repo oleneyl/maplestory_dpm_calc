22d1690fb72cc7c623f95369c9346b048b4a257e
ha <typ0@naver.com>
2020-08-02 18:24:52 +0900

    fix: 블레이드 토네이도(소환)에는 히든 블레이드 미적용

--- dpmModule/jobs/dualblade.py
+++ dpmModule/jobs/dualblade.py
@@ -113,7 +113,7 @@ class JobGenerator(ck.JobGenerator):
         HiddenBladeOpt = core.OptionalElement(HiddenBladeBuff.is_active, HiddenBlade)
         
         FlashBang.onAfter(FlashBangDebuff)
-        for sk in [FinalCut, PhantomBlow, SuddenRaid, FlashBang, AsuraTick, BladeStorm, BladeStormTick, BladeTornado, BladeTornadoSummon]:
+        for sk in [FinalCut, PhantomBlow, SuddenRaid, FlashBang, AsuraTick, BladeStorm, BladeStormTick, BladeTornado]:
             sk.onAfter(HiddenBladeOpt)
             
         for sk in [PhantomBlow, AsuraTick, BladeStormTick]:
