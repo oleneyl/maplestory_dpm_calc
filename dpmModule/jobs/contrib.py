24360614656b3931c61991fcc1ecd35c166869b6
cheese98 <cheesej98@gmail.com>
2020-08-04 07:59:08 +0900

    create_auxilary_attack에 이름 매개변수 추가

--- dpmModule/jobs/contrib.py
+++ dpmModule/jobs/contrib.py
@@ -1,8 +1,8 @@
 from ..kernel import core
 from ..kernel.graph import DynamicVariableOperation
-def create_auxilary_attack(skill_wrapper, ratio):
+def create_auxilary_attack(skill_wrapper, ratio, nametag = '복사'):
     original_skill = skill_wrapper.skill
-    copial_skill = core.DamageSkill(name = DynamicVariableOperation.reveal_argument(original_skill.name) + '(복사)',
+    copial_skill = core.DamageSkill(name = DynamicVariableOperation.reveal_argument(original_skill.name) + nametag,
         delay = DynamicVariableOperation.wrap_argument(0),
         damage = original_skill.damage * DynamicVariableOperation.wrap_argument(ratio),
         hit = original_skill.hit,
