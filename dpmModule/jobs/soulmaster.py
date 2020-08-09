6b84f0fcd74956530673ba4aa83fc1e816c2c1b1
ha <typ0@naver.com>
2020-08-09 22:08:56 +0900

    fix: 소울마스터 스킬
    * 엘리멘탈 엑스퍼트, 엘리멘탈 하모니 효과 뒤바뀜
    * 라이징 선 / 폴링 문 효과 정확히 적용
    * 셀레스티얼 추가타 코강 우선순위 셀레스티얼과 일치시킴
    * 소울 이클립스 퍼뎀 수정
    * 솔루나 디바이드에 딜레이 추가
    * 엘리시온 균열 3번만 터지게 수정
    * 오라 웨폰에 폴링 문 적용

--- dpmModule/jobs/soulmaster.py
+++ dpmModule/jobs/soulmaster.py
@@ -29,11 +29,11 @@ class JobGenerator(ck.JobGenerator):
         chtr.add_property_ignorance(10)
 
     def get_modifier_optimization_hint(self):
-        return core.CharacterModifier(crit = 35, pdamage = 55)
+        return core.CharacterModifier(pdamage = 20)
 
     def get_passive_skill_list(self):
-        ElementalExpert = core.InformedCharacterModifier("엘리멘탈 엑스퍼트",stat_main = self.chtr.level // 2)
-        ElementalHarmony = core.InformedCharacterModifier("엘리멘탈 하모니",patt = 10)
+        ElementalExpert = core.InformedCharacterModifier("엘리멘탈 엑스퍼트", patt = 10)
+        ElementalHarmony = core.InformedCharacterModifier("엘리멘탈 하모니", stat_main = self.chtr.level // 2)
         
         SwordOfLight = core.InformedCharacterModifier("소드 오브 라이트",att = 20)
         Soul = core.InformedCharacterModifier("소울",armor_ignore = 10)
@@ -58,35 +58,34 @@ class JobGenerator(ck.JobGenerator):
         '''
         하이퍼 : 
         트루사이트 : 내성무시 / 방무
-        보스전 딜스킬 : 리인포스 /이그노어 가드/보스킬러
+        보스전 딜스킬 : 리인포스 / 이그노어 가드 / 보스킬러
         '''
-        MasterOfSword = core.CharacterModifier(crit = 35, pdamage_indep = 12.5, att=45)  #타수2배 , 공격력 90%를 편집함
+        FallingMoon = core.CharacterModifier(pdamage_indep = -10)
 
         #Buff skills
-        NimbleFinger = core.BuffSkill("님블 핑거", 0, 180 * 1000, rem = True).wrap(core.BuffSkillWrapper)    #딜레이 모름
-        TrueSight = core.BuffSkill("트루 사이트", 600, 30 * 1000, armor_ignore = 10+10, pdamage_indep = 5).wrap(core.BuffSkillWrapper)   #딜레이 모름, 방10/지속20s/내성10?
-        SolunaTime = core.BuffSkill("솔루나 타임", 600, 200 * 1000, rem = True).wrap(core.BuffSkillWrapper)  #딜레이 모름
-        SoulForge = core.BuffSkill("소울 포지", 600, 180 * 1000, att = 50, rem = True).wrap(core.BuffSkillWrapper)      #딜레이 모름
+        NimbleFinger = core.BuffSkill("님블 핑거", 0, 180 * 1000, rem = True).wrap(core.BuffSkillWrapper) # 펫버프
+        TrueSight = core.BuffSkill("트루 사이트", 990, 30 * 1000, armor_ignore = 10+10, pdamage_indep = 5).wrap(core.BuffSkillWrapper) # 내성무시는 complex_option에 있음
+        SolunaTime = core.BuffSkill("솔루나 타임", 0, 200 * 1000, rem = True, crit = 35, pdamage_indep = 25, att = 45).wrap(core.BuffSkillWrapper)  # 딜레이 없음
+        SoulForge = core.BuffSkill("소울 포지", 0, 180 * 1000, att = 50, rem = True).wrap(core.BuffSkillWrapper) # 펫버프
         GloryOfGuardians = core.BuffSkill("글로리 오브 가디언즈", 0, 60*1000, cooltime = 120 * 1000, pdamage = 10).wrap(core.BuffSkillWrapper)
     
         #Damage Skills
-        NormalAttack = core.DamageSkill("댄스오브 문/스피딩 선셋", (360+270)/2, 400, 4 * 2, modifier = core.CharacterModifier(pdamage = 20, boss_pdamage = 20, armor_ignore = 20) + MasterOfSword.copy()).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
+        NormalAttack = core.DamageSkill("댄스오브 문/스피딩 선셋", (360+270)/2, 400, 4 * 2, modifier = core.CharacterModifier(pdamage = 20, boss_pdamage = 20, armor_ignore = 20) + FallingMoon.copy()).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
         
         CygnusPalanks = cygnus.PhalanxChargeWrapper(vEhc, 4, 4)
         
+        SelestialDanceInit = core.BuffSkill("셀레스티얼 댄스", 700, (40+vEhc.getV(0,0))*1000, cooltime = 150 * 1000, red = True).isV(vEhc,0,0).wrap(core.BuffSkillWrapper)
         SelestialDanceSummon = core.SummonSkill("셀레스티얼 댄스(추가타)", 0, 5000, (1200 + 40 * vEhc.getV(0,0)), 3, (40 + vEhc.getV(0,0)) * 1000, cooltime = -1).isV(vEhc,0,0).wrap(core.SummonSkillWrapper) #딜레이 모름
-        SelestialDanceInit = core.BuffSkill("셀레스티얼 댄스", 700, (40+vEhc.getV(0,0))*1000, cooltime = 150000, red = True).isV(vEhc,0,0).wrap(core.BuffSkillWrapper)
-
-        SelestialDanceAttack = core.DamageSkill("댄스오브 문/스피딩 선셋(셀레스티얼)", 0, 400*0.01*(30+vEhc.getV(0,2)), 4 * 2, modifier = core.CharacterModifier(pdamage = 20, boss_pdamage = 20, armor_ignore = 20)+MasterOfSword.copy()).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)    #직접사용 X
+        SelestialDanceAttack = core.DamageSkill("댄스오브 문/스피딩 선셋(셀레스티얼)", 0, 400*0.01*(30+vEhc.getV(0,0)), 4 * 2, modifier = core.CharacterModifier(pdamage = 20, boss_pdamage = 20, armor_ignore = 20) + FallingMoon.copy()).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)    #직접사용 X
         
         #엘리시온 38타 / 3타
-        ElisionBreak = core.DamageSkill("엘리시온(균열)", 0, 2600 + 104*vEhc.getV(1,1), 12).isV(vEhc,1,1).wrap(core.DamageSkillWrapper)    #3회 발동
-        ElisionTick = core.DamageSkill("크로스 더 스틱스(엘리시온에 의해 발동)", 30 * 1000 / 40, 1450, 5 * 2, modifier = MasterOfSword.copy()).isV(vEhc,1,1).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)  #40회 반복
         ElisionBuff = core.BuffSkill("엘리시온", 750, 30 * 1000, cooltime = 180 * 1000).isV(vEhc,1,1).wrap(core.BuffSkillWrapper)    #시전딜레이 750ms
+        ElisionBreak = core.DamageSkill("엘리시온(균열)", 0, 2600 + 104*vEhc.getV(1,1), 12).isV(vEhc,1,1).wrap(core.DamageSkillWrapper)    #3회 발동
+        ElisionTick = core.DamageSkill("크로스 더 스틱스(엘리시온에 의해 발동)", 30 * 1000 / 40, 1450, 5 * 2, modifier = FallingMoon.copy()).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)  #40회 반복
         
         #소울 이클립스
-        SoulEclipse = core.SummonSkill("소울 이클립스", 810, 1000, 510 + 8 * vEhc.getV(3,3), 7, 30 * 1000, cooltime = 180 * 1000).isV(vEhc,3,3).wrap(core.SummonSkillWrapper)
-        SolunaDivide = core.DamageSkill("솔루나 디바이드", 0, 1250 + 50 * vEhc.getV(3,3), 15 * 5, cooltime = 180 * 1000).isV(vEhc,3,3).wrap(core.DamageSkillWrapper)
+        SoulEclipse = core.SummonSkill("소울 이클립스", 810, 1000, 450 + 18 * vEhc.getV(3,3), 7, 30 * 1000, cooltime = 180 * 1000).isV(vEhc,3,3).wrap(core.SummonSkillWrapper)
+        SolunaDivide = core.DamageSkill("솔루나 디바이드", 990, 1250 + 50 * vEhc.getV(3,3), 15 * 5, cooltime = 180 * 1000).isV(vEhc,3,3).wrap(core.DamageSkillWrapper)
         
         ######   Skill Wrapper   ######
         
@@ -98,14 +97,14 @@ class JobGenerator(ck.JobGenerator):
         NormalAttack.onAfter(SelecstialDanceOption)
         
         Elision = core.RepeatElement(ElisionTick, 40)
-        Elision.onAfter(core.RepeatElement(ElisionBreak, 4))
+        Elision.onAfter(core.RepeatElement(ElisionBreak, 3))
         ElisionBuff.onAfter(Elision)
     
         SolunaDivide.set_disabled_and_time_left(-1)
         SoulEclipse.onAfter(SolunaDivide.controller(30*1000))
 
         # 오라 웨폰
-        auraweapon_builder = warriors.AuraWeaponBuilder(vEhc, 2, 2)
+        auraweapon_builder = warriors.AuraWeaponBuilder(vEhc, 2, 2, modifier=FallingMoon.copy(), hit=6*2)
         for sk in [NormalAttack, ElisionTick]:
             auraweapon_builder.add_aura_weapon(sk)
         AuraWeaponBuff, AuraWeaponCooltimeDummy = auraweapon_builder.get_buff()
