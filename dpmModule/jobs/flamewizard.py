b0782e4e61c27149a780203b03285d8f8495889a
ha <typ0@naver.com>
2020-08-10 03:05:33 +0900

    fix: 플위 스킬
    * 패시브 이름들 한글화
    * 인페르노라이즈 뎀퍼로 잘못 적용된것 최종뎀으로
    * 디스차지 타수 수정
    * 디스차지 4스택 경우 추가
    * 인플서 전분 기반으로 타수 변경
    * 인페르노라이즈 사용 타이밍 수정

--- dpmModule/jobs/flamewizard.py
+++ dpmModule/jobs/flamewizard.py
@@ -23,17 +23,15 @@ class JobGenerator(ck.JobGenerator):
 
     def get_passive_skill_list(self):
         ######   Skill   ######
-        ElementalExpert = core.InformedCharacterModifier("엘리멘탈 엑스퍼트",stat_main = self.chtr.level // 2)
-        ElementalHarmony = core.InformedCharacterModifier("엘리멘탈 하모니",patt = 10)     
+        ElementalExpert = core.InformedCharacterModifier("엘리멘탈 엑스퍼트", patt = 10)
+        ElementalHarmony = core.InformedCharacterModifier("엘리멘탈 하모니", stat_main = self.chtr.level // 2)
         
-        SpellControl = core.InformedCharacterModifier("스펠 컨트롤",att = 10)
-        #이그니션 : 매초 (220% * 1.8) x2-> 폭발하여 240%
+        SpellControl = core.InformedCharacterModifier("주문 연마",att = 10)
         LiberatedMagic = core.InformedCharacterModifier("해방된 마력",pdamage_indep = 30)
-        BurningFocus = core.InformedCharacterModifier("버닝 포커스",crit = 30, crit_damage = 15)
-        BriliantEnlightenment = core.InformedCharacterModifier("briliantEnlightenment",stat_main = 60)
-        PureMagic = core.InformedCharacterModifier("순수한 마력", att = 20, pdamage_indep = 50)
-        #TODO
-        #Elemental Reset --> elemental ignorance +10% --> first we will apply this term as simple pdamage_indep
+        BurningFocus = core.InformedCharacterModifier("약점 분석",crit = 30, crit_damage = 15)
+        BriliantEnlightenment = core.InformedCharacterModifier("번뜩이는 깨달음",stat_main = 60)
+        PureMagic = core.InformedCharacterModifier("마법의 진리", att = 20, pdamage_indep = 50)
+
         return [ElementalExpert, ElementalHarmony, SpellControl, LiberatedMagic, BurningFocus, BriliantEnlightenment, PureMagic]
 
     def get_not_implied_skill_list(self):
@@ -52,6 +50,7 @@ class JobGenerator(ck.JobGenerator):
         '''
         
         flamewizardDefaultSpeed = 60000 / ((1350) / 6)  #266
+        blazingOrbitalHit = 4
         
         #Buff skills
         WordOfFire = core.BuffSkill("북 오브 파이어", 0, 300000, att = 20).wrap(core.BuffSkillWrapper)
@@ -61,12 +60,13 @@ class JobGenerator(ck.JobGenerator):
         
         OverloadMana = OverloadMana = magicians.OverloadManaWrapper(vEhc, 1, 2)
         #Damage Skills
-        InfernoRize = core.DamageSkill("인페르노라이즈", 540, 350, 10, cooltime = 30*1000, modifier = core.CharacterModifier(pdamage = 90)).setV(vEhc, 4, 2, False).wrap(core.DamageSkillWrapper)    #임의딜레이 720
+        InfernoRize = core.DamageSkill("인페르노라이즈", 540, 350, 10, cooltime = 30*1000, modifier = core.CharacterModifier(pdamage_indep = 90)).setV(vEhc, 4, 2, False).wrap(core.DamageSkillWrapper)    #임의딜레이 720
         
         #Full speed, No Combat Orders
         OrbitalFlame = core.DamageSkill("오비탈 플레임 IV", flamewizardDefaultSpeed, 215, 3 * 2, modifier = core.CharacterModifier(armor_ignore = 20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
+        # BlazingExtinction = core.SummonSkill("블레이징 익스팅션", 1020, 2500, 310, 3+1, 10000, cooltime=5000, modifier = core.CharacterModifier(pdamage = 20)).wrap(core.SummonSkillWrapper)
         CygnusPalanks = cygnus.PhalanxChargeWrapper(vEhc, 2, 1)
-        BlazingOrbital = core.DamageSkill("블레이징 오비탈 플레임", 210, 330+13*vEhc.getV(0,0), 6 * 4, cooltime = 5000, modifier = core.CharacterModifier(armor_ignore = 50)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)    #4타 가정
+        BlazingOrbital = core.DamageSkill("블레이징 오비탈 플레임", 210, 330+13*vEhc.getV(0,0), 6 * blazingOrbitalHit, cooltime = 5000, modifier = core.CharacterModifier(armor_ignore = 50)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)    #4타 가정
         
         DragonSlaveTick = core.DamageSkill("드래곤 슬레이브", 280, 500, 6).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)#x7
         DragonSlaveInit = core.DamageSkill("드래곤 슬레이브 개시(더미)", 0, 0, 0, cooltime = 90 * 1000).wrap(core.DamageSkillWrapper)
@@ -78,8 +78,9 @@ class JobGenerator(ck.JobGenerator):
         
         SavageFlame = core.DamageSkill("플레임 디스차지", 0, 0, 0, cooltime = 20*1000).isV(vEhc,4,4).wrap(core.DamageSkillWrapper)
     
-        SavageFlame_2 = core.DamageSkill("플레임 디스차지(2)", 840, 250 + 10*vEhc.getV(4,4), 8*(8+2)).isV(vEhc,4,4).wrap(core.DamageSkillWrapper)
-        SavageFlame_3 = core.DamageSkill("플레임 디스차지(3)", 840, 250 + 10*vEhc.getV(4,4), 8*(8+3)).isV(vEhc,4,4).wrap(core.DamageSkillWrapper)
+        SavageFlame_2 = core.DamageSkill("플레임 디스차지(2스택)", 840, 250 + 10*vEhc.getV(4,4), 8*8).isV(vEhc,4,4).wrap(core.DamageSkillWrapper)
+        SavageFlame_3= core.DamageSkill("플레임 디스차지(3스택)", 840, 250 + 10*vEhc.getV(4,4), 8*(8+2)).isV(vEhc,4,4).wrap(core.DamageSkillWrapper)
+        SavageFlame_4 = core.DamageSkill("플레임 디스차지(4스택)", 840, 250 + 10*vEhc.getV(4,4), 8*(8+2+2)).isV(vEhc,4,4).wrap(core.DamageSkillWrapper)
         
         InfinityFlameCircleTick = core.DamageSkill("인피니티 플레임 서클", 180, 500+20*vEhc.getV(3,3),7, modifier = core.CharacterModifier(crit = 50, armor_ignore = 50)).isV(vEhc,3,3).wrap(core.DamageSkillWrapper) #1틱
         InfinityFlameCircleInit = core.DamageSkill("인피니티 플레임 서클(개시)", 360, 0, 0, cooltime = 15*12*1000).isV(vEhc,3,3).wrap(core.DamageSkillWrapper)#계산 필요
@@ -90,14 +91,19 @@ class JobGenerator(ck.JobGenerator):
         DragonSlave.onAfter(DragonSlaveEnd)
         DragonSlaveInit.onAfter(DragonSlave)
     
-        InfinityFlameCircle = core.RepeatElement(InfinityFlameCircleTick, 83)
+        InfinityFlameCircle = core.RepeatElement(InfinityFlameCircleTick, 79)
         
         InfinityFlameCircleInit.onAfter(InfinityFlameCircle)
         
         IgnitionDOT.onAfter(SavageFlameStack.stackController(1))
         InfernoRize.onAfter(IgnitionDOT.controller(1))
+        DragonSlaveEnd.onAfter(IgnitionDOT.controller(1))
 
-        SavageFlame.onAfter( core.OptionalElement(partial(SavageFlameStack.judge, 3, 1), SavageFlame_3, SavageFlame_2, name = "스택 확인") )
+        InfernoRize.onConstraint(core.ConstraintElement("이그니션 시간 체크", IgnitionDOT, partial(IgnitionDOT.is_time_left, 9000, 1)))
+
+        StackCheck3 = core.OptionalElement(partial(SavageFlameStack.judge, 3, 1), SavageFlame_3, SavageFlame_2, name = "스택 확인")
+        StackCheck4 = core.OptionalElement(partial(SavageFlameStack.judge, 4, 1), SavageFlame_4, StackCheck3, name = "스택 확인")
+        SavageFlame.onAfter(StackCheck4)
         SavageFlame.onAfter(SavageFlameStack.stackController(-15))
 
         schedule = core.ScheduleGraph()
