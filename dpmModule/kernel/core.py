b4dde0a472e87595529ecad5ad6cbf549a5cd40d
oleneyl <39217532+oleneyl@users.noreply.github.com>
2020-08-11 18:45:04 +0900

    Revert "Change execution order of after, before"

--- dpmModule/kernel/core.py
+++ dpmModule/kernel/core.py
@@ -777,16 +777,16 @@ class GraphElement():
         self._run += ellist
         
     def onAfter(self, el):
-        self._after = self._after + [el]
+        self._after = [el] + self._after 
         
     def onAfters(self, ellist : list):
         self._after = self._after  + ellist
         
     def onBefore(self, el):
-        self._before = [el] + self._before
+        self._before = self._before + [el]
         
     def onBefores(self, ellist : list):
-        self._before = ellist + self._before
+        self._before += ellist
         
     def sync(self, task):
         task.onBefore([el.build_task() for el in self._before])
