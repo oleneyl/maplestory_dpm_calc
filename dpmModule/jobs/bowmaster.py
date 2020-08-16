9573b3b382293565710a37cafcc07b6c8ffb1dc4
ha <typ0@naver.com>
2020-08-17 05:05:31 +0900

    fix: 보우마스터 스킬
    * 모탈 블로우, 아머 피어싱을 폭풍의 시에 통합해서 적용
      * 윈드 오브 프레이에도 적용되나, 딜사이클에 안들어가므로 이와 같이 구현함
    * 윈드 오브 프레이 스킬 생성
      * 딜사이클에 들어가진 않음
    * 잔영의 시 액티브 중엔 패시브 발동 안되게 고침

--- dpmModule/jobs/bowmaster.py
+++ dpmModule/jobs/bowmaster.py
@@ -7,7 +7,35 @@ from . import globalSkill
 from .jobbranch import bowmen
 
 #TODO : 5차 신스킬 적용
-#TODO : 윈드 오브 프레이 추가
+class ArrowOfStormWrapper(core.DamageSkillWrapper):
+    '''
+    모탈 블로우 - 9회 공격 후 다음 공격은 데미지+80%
+    아머 피어싱 - 적 방어율만큼 최종뎀 추가, 방무+50%. 쿨타임 9초, 공격마다 1초씩 감소, 최소 재발동 대기시간 1초
+    '''
+    def __init__(self, skill):
+        super(ArrowOfStormWrapper, self).__init__(skill)
+        self.mortalBlowCount = 0
+        self.armorPiercingCooltime = 0
+
+    def spend_time(self, time):
+        self.armorPiercingCooltime -= time
+        super(ArrowOfStormWrapper, self).spend_time(time)
+        
+    def _use(self, rem = 0, red = 0):
+        modifier = self.get_modifier()
+        if self.mortalBlowCount >= 9:
+            modifier += core.CharacterModifier(pdamage = 80)
+            self.mortalBlowCount = 0
+        else:
+            self.mortalBlowCount += 1
+
+        if self.armorPiercingCooltime <= 0:
+            modifier += core.CharacterModifier(pdamage_indep = 300, armor_ignore = 50) # 적 방어율 300 가정
+            self.armorPiercingCooltime = 9000 * (1 - 0.01 * red)
+        elif self.armorPiercingCooltime > 1000:
+            self.armorPiercingCooltime = max(self.armorPiercingCooltime - 1000, 1000)
+        
+        return core.ResultObject(self.skill.delay, modifier, self.skill.damage, self.skill.hit, sname = self.skill.name, spec = self.skill.spec)
 
 class JobGenerator(ck.JobGenerator):
     def __init__(self):
@@ -17,6 +45,9 @@ class JobGenerator(ck.JobGenerator):
         self.vEnhanceNum = 11
         self.ability_list = Ability_tool.get_ability_set('boss_pdamage', 'crit', 'buff_rem')
         self.preEmptiveSkills = 1
+
+    def get_modifier_optimization_hint(self):
+        return core.CharacterModifier(pdamage = 18)
 
     def get_passive_skill_list(self):
         CriticalShot = core.InformedCharacterModifier("크리티컬 샷",crit = 40)
@@ -35,11 +66,8 @@ class JobGenerator(ck.JobGenerator):
         Mastery = core.InformedCharacterModifier("숙련도",pdamage_indep = -7.5)
 
         ExtremeArchery = core.InformedCharacterModifier("익스트림 아처리",att = 40, pdamage_indep = 30)
-        MortalBlow = core.InformedCharacterModifier("모탈 블로우",pdamage = 80/10)   #확률성을 변형적용... 패치 필요할듯
-
-        ArmorPiercing = core.InformedCharacterModifier("아머 피어싱",pdamage_indep = 100/8, armor_ignore = 50/8) # 가동률 약 1/8, 잠재 쿨감 1초당 분모 0.5씩 빼야함
         
-        return [WeaponConstant, Mastery,  MortalBlow, ExtremeArchery, ArmorPiercing]
+        return [WeaponConstant, Mastery, ExtremeArchery]
 
     def generate(self, vEhc, chtr : ck.AbstractCharacter, combat : bool = False):
         '''
@@ -61,18 +89,16 @@ class JobGenerator(ck.JobGenerator):
         Preparation = core.BuffSkill("프리퍼레이션", 900, 30 * 1000, cooltime = 90 * 1000, att = 50, boss_pdamage = 20).wrap(core.BuffSkillWrapper)
         EpicAdventure = core.BuffSkill("에픽 어드벤처", 0, 60*1000, cooltime = 120 * 1000, pdamage = 10).wrap(core.BuffSkillWrapper)
     
-        #ArmorPiercing = core.BuffSkill("아머 피어싱", ?, ?) #쿨 9초, 적 방어율만큼 최종뎀 1회 한정 증가, 방무 50%, 타격마다 1초 감소, 쿨타임 최대 감소량 1초
-        
         #Damage Skills
         AdvancedQuibberAttack = core.DamageSkill("마법 화살", 0, 260, 0.6).setV(vEhc, 3, 2, True).wrap(core.DamageSkillWrapper)
         AdvancedQuibberAttack_ArrowRain = core.DamageSkill("마법 화살(애로우 레인)", 0, 260, 1).setV(vEhc, 3, 2, True).wrap(core.DamageSkillWrapper)
         AdvancedFinalAttack = core.DamageSkill("어드밴스드 파이널 어택", 0, 210, 0.7).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
         
-        ArrowOfStorm = core.DamageSkill("폭풍의 시", 120, 350*0.75, 1+1, modifier = core.CharacterModifier(pdamage = 30, boss_pdamage = 10)).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)  #오더스 적용 필요, 아머 피어싱, 
+        ArrowOfStorm = ArrowOfStormWrapper(core.DamageSkill("폭풍의 시", 120, (350+combat*3)*0.75, 1+1, modifier = core.CharacterModifier(pdamage = 30, boss_pdamage = 10)).setV(vEhc, 0, 2, True))
         ArrowFlatter = core.SummonSkill("애로우 플래터", 600, 210, 85+90+combat*3, 1, 30 * 1000, modifier = core.CharacterModifier(pdamage = 30)).setV(vEhc, 4, 2, False).wrap(core.SummonSkillWrapper) # 딜레이 모름
-        # 작성중
-        #GrittyGust = core.DamageSkill("윈드 오브 프레이", ?, 500, 81, cooltime = 15 * 1000, red = true).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
-        #GrittyGustDOT = core.SummonSkill("윈드 오브 프레이(도트)", 0, 1000, 200, 1, 10*1000, cooltime = -1).isV(vEhc,0,0).wrap(core.SummonSkillWrapper)
+        
+        GrittyGust = core.DamageSkill("윈드 오브 프레이", 720, 500, 8, cooltime = 15 * 1000).setV(vEhc, 6, 2, True).wrap(core.DamageSkillWrapper)
+        GrittyGustDOT = core.DotSkill("윈드 오브 프레이(도트)", 200, 10*1000).wrap(core.SummonSkillWrapper)
         
         ArrowRainBuff = core.BuffSkill("애로우 레인(버프)", 810, (40+vEhc.getV(0,0))*1000, cooltime = 120 * 1000, red = True, pdamage = 15+(vEhc.getV(0,0)//2)).isV(vEhc,0,0).wrap(core.BuffSkillWrapper)
         ArrowRain = core.SummonSkill("애로우 레인", 0, 1440, 600+vEhc.getV(0,0)*24, 8, (40+vEhc.getV(0,0))*1000, cooltime = -1).isV(vEhc,0,0).wrap(core.SummonSkillWrapper) # 5초마다 3.5회 공격, 대략 1440ms당 1회
@@ -91,17 +117,17 @@ class JobGenerator(ck.JobGenerator):
         ImageArrowPassive = core.DamageSkill("잔영의 시(패시브)", 0, 400+16*vEhc.getV(1,1), 3 * 2 * 0.1).isV(vEhc,1,1).wrap(core.DamageSkillWrapper) # 10회당 1번 생성, 생성당 3타씩 2회 공격
     
         ######   Skill Wrapper   ######
-        #GrittyGust.onAfter(GrittyGustDOT)
+        GrittyGust.onAfter(GrittyGustDOT)
+
         #이볼브 연계 설정
         Evolve.onAfter(Pheonix.controller(1))
         Pheonix.onConstraint(core.ConstraintElement("이볼브 사용시 사용 금지", Evolve, Evolve.is_not_active))
             
-    
         CriticalReinforce = bowmen.CriticalReinforceWrapper(vEhc, chtr, 3, 3, 22.5) #Maybe need to sync
-    
+
         ArrowOfStorm.onAfter(AdvancedQuibberAttack)
         ArrowOfStorm.onAfter(AdvancedFinalAttack)
-        ArrowOfStorm.onAfter(ImageArrowPassive)
+        ArrowOfStorm.onAfter(core.OptionalElement(ImageArrow.is_not_active, ImageArrowPassive, name="액티브 OFF"))
         
         ArrowRain.onTicks([AdvancedFinalAttack, AdvancedQuibberAttack_ArrowRain])
         ImageArrow.onTicks([AdvancedFinalAttack, AdvancedQuibberAttack])
@@ -110,12 +136,10 @@ class JobGenerator(ck.JobGenerator):
         QuibberFullBurstBuff.onAfter(QuibberFullBurstDOT)
         QuibberFullBurstBuff.onAfter(QuibberFullBurst)
 
-        ImageArrowPassive.onConstraint(core.ConstraintElement("액티브 OFF", ImageArrow, ImageArrow.is_not_active))
-    
         ### Exports ###
         return(ArrowOfStorm,
                 [globalSkill.maple_heros(chtr.level),
-                    SoulArrow, AdvancedQuibber, SharpEyes, ElusionStep, Preparation, EpicAdventure, ArrowRainBuff, CriticalReinforce, QuibberFullBurstBuff, 
+                    SoulArrow, AdvancedQuibber, SharpEyes, ElusionStep, Preparation, EpicAdventure, ArrowRainBuff, CriticalReinforce, QuibberFullBurstBuff,
                     globalSkill.soul_contract()] +\
                 [] +\
                 [Evolve, ArrowFlatter, ArrowRain, Pheonix, GuidedArrow, QuibberFullBurst, ImageArrow] +\
