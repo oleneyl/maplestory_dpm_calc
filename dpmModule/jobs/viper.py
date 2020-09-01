9a4c70c506ab99c01e00da27e00f82a378f71a57
cheese98 <cheesej98@gmail.com>
2020-09-01 10:54:13 +0900

    바이퍼 컴뱃오더스 적용 (다이스 제외)

--- dpmModule/jobs/viper.py
+++ dpmModule/jobs/viper.py
@@ -7,22 +7,22 @@ from . import globalSkill
 from .jobbranch import pirates
 from .jobclass import adventurer
 from . import jobutils
+from math import ceil
 #TODO : 5차 신스킬 적용
 
 class EnergyChargeWrapper(core.StackSkillWrapper):
-    def __init__(self):
+    def __init__(self, combat):
         skill = core.BuffSkill("에너지차지 발동", 0, 999999*1000)
         super(EnergyChargeWrapper, self).__init__(skill, 10000)
         self.stack = 0
         self._st = 0
-
+        self.combat = combat
     def vary(self, val, force = False):
-#        print(" State : %d, %d \n vary %d" % (self.stack, self._st, val))
+        #print(" State : %d, %d \n vary %d" % (self.stack, self._st, val))
         if (force or not self._st) and val > 0:
             self.stack += val
         elif val < 0:
             self.stack += val
-            
         if self._st and self.stack <= 0:
             self.turnOff()
         elif (not self._st) and self.stack >= 10000:
@@ -31,7 +31,7 @@ class EnergyChargeWrapper(core.StackSkillWrapper):
             
     def turnOn(self):
         self.stack = 10000
-        self.skill.att = 50
+        self.skill.att = 50 + 2 * self.combat
         self._st = 1
 
     def turnOff(self):
@@ -42,7 +42,6 @@ class EnergyChargeWrapper(core.StackSkillWrapper):
     def isStateOn(self):
         return self._st
 
-
 class JobGenerator(ck.JobGenerator):
     def __init__(self):
         super(JobGenerator, self).__init__()
@@ -51,6 +50,7 @@ class JobGenerator(ck.JobGenerator):
         self.jobname = "바이퍼"
         self.ability_list = Ability_tool.get_ability_set('boss_pdamage', 'crit', 'buff_rem')
         self.preEmptiveSkills = 1
+        self._combat = 0
 
     def get_passive_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
         CriticalRoar = core.InformedCharacterModifier("크리티컬 로어",crit = 20, crit_damage = 5)
@@ -69,13 +69,14 @@ class JobGenerator(ck.JobGenerator):
         return [CriticalRoar, MentalClearity, PhisicalTraining, CriticalRage, StimulatePassive, EchoOfHero, LoadedDicePassive]
 
     def get_not_implied_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
+        passive_level = chtr.get_base_modifier().passive_level + self._combat
+
         WeaponConstant = core.InformedCharacterModifier("무기상수",pdamage_indep = 70)
-        Mastery = core.InformedCharacterModifier("숙련도",pdamage_indep = -5)
+        Mastery = core.InformedCharacterModifier("숙련도",pdamage_indep = -5 + 0.5*ceil(self._combat/2))
 
         CriticalRage = core.InformedCharacterModifier("크리티컬 레이지(준액티브)",crit = 20)    #보스상대 추가+20% 크리율
-        GuardCrush = core.InformedCharacterModifier("가드 크러시",armor_ignore = 40) #40% 확률로 방무 100% 무시.
-        CounterAttack = core.InformedCharacterModifier("카운터 어택",pdamage = 25)  #피격시 25%로발동.
-        
+        GuardCrush = core.InformedCharacterModifier("가드 크러시",armor_ignore = 40 + 2*passive_level) #40% 확률로 방무 100% 무시.
+        CounterAttack = core.InformedCharacterModifier("카운터 어택",pdamage = 25 + 2*passive_level)  #피격시 25%로발동.
         
         return [WeaponConstant, Mastery, CriticalRage, GuardCrush, CounterAttack]
         
@@ -86,13 +87,14 @@ class JobGenerator(ck.JobGenerator):
         인레이지-노틸-드스-유니티
         '''
         ######   Skill   ######
+        passive_level = chtr.get_base_modifier().passive_level + self._combat
         serverlag = 3
 
         LuckyDice = core.BuffSkill("로디드 다이스", 600, 180 * 1000, pdamage = 20 *4/3).isV(vEhc, 2, 2).wrap(core.BuffSkillWrapper)#딜레이 모름
         #1중첩 럭다 재사용 50초 감소 / 방어력30% / 체엠 20% / 크리율15% / 뎀증20 / 경치30
         #2중첩 럭다 재사용 50초 감소 / 방어력40% / 체엠 30% / 크리율25% / 뎀증30 / 경치40
         #7 발동시 방무 20 -> 30
-        Viposition = core.BuffSkill("바이퍼지션", 0, 180 * 1000, patt = 30).wrap(core.BuffSkillWrapper)
+        Viposition = core.BuffSkill("바이퍼지션", 0, (180+4*self._combat) * 1000, patt = 30+self._combat).wrap(core.BuffSkillWrapper)
         Stimulate = core.BuffSkill("스티뮬레이트", 930, 120 * 1000, cooltime = 240 * 1000, pdamage = 20).wrap(core.BuffSkillWrapper)# 에너지 주기적으로 800씩 증가, 미완충시 풀완충.
         StimulateSummon = core.SummonSkill("스티뮬레이트(게이지 증가 더미)", 0, (5 + serverlag) * 1000, 0, 0, 120 * 1000).wrap(core.SummonSkillWrapper)
         
@@ -111,15 +113,15 @@ class JobGenerator(ck.JobGenerator):
         Transform = core.BuffSkill("트랜스 폼", 450, (50+vEhc.getV(1,1))*1000, cooltime = 180 * 1000, pdamage_indep = (20 + 0.2*vEhc.getV(1,1))).isV(vEhc,1,1).wrap(core.BuffSkillWrapper)#에너지 완충
         TransformEnergyOrb = core.DamageSkill("에너지 오브", 1140, 450 +vEhc.getV(1,1)*18, (2+(vEhc.getV(1,1) == 25)*1) * 8, modifier = core.CharacterModifier(crit = 50, armor_ignore = 50)).isV(vEhc,1,1).wrap(core.DamageSkillWrapper)
     
-        FistInrage = core.DamageSkill("피스트 인레이지", 690, 320, 8 + 1, modifier = core.CharacterModifier(boss_pdamage = 20, pdamage = 20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
-        FistInrage_T = core.DamageSkill("피스트 인레이지(변신)", 690, 320, 8 + 1 + 2, modifier = core.CharacterModifier(boss_pdamage = 20, pdamage = 20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)#완충시 10번 공격
+        FistInrage = core.DamageSkill("피스트 인레이지", 690, 320 + 4*self._combat, 8 + 1, modifier = core.CharacterModifier(boss_pdamage = 20, pdamage = 20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
+        FistInrage_T = core.DamageSkill("피스트 인레이지(변신)", 690, 320+4*self._combat, 8 + 1 + 2, modifier = core.CharacterModifier(boss_pdamage = 20, pdamage = 20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)#완충시 10번 공격
         
-        DragonStrike = core.DamageSkill("드래곤 스트라이크", 690, 300, 12, cooltime = 15 * 1000).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)
-        DragonStrikeBuff = core.BuffSkill("드래곤 스트라이크(디버프)", 0, 15 * 1000, cooltime = -1, pdamage_indep = 20).wrap(core.BuffSkillWrapper)
+        DragonStrike = core.DamageSkill("드래곤 스트라이크", 690, 300 + 4*self._combat, 12, cooltime = 15 * 1000).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)
+        DragonStrikeBuff = core.BuffSkill("드래곤 스트라이크(디버프)", 0, 15 * 1000, cooltime = -1, pdamage_indep = 20 + self._combat//2).wrap(core.BuffSkillWrapper)
         
-        Nautilus = core.DamageSkill("노틸러스", 690, 440, 7, cooltime = 60 * 1000).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
+        Nautilus = core.DamageSkill("노틸러스", 690, 440+4*self._combat, 7, cooltime = 60 * 1000).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
         NautilusBuff = core.BuffSkill("노틸러스(버프)", 0, 60 * 1000, cooltime = -1).wrap(core.BuffSkillWrapper)
-        NautilusFinalAttack = core.DamageSkill("노틸러스(파이널 어택)", 0, 165, 2).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
+        NautilusFinalAttack = core.DamageSkill("노틸러스(파이널 어택)", 0, 165+2*self._combat, 2).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
         
         SerpentScrew = core.SummonSkill("서펜트 스크류", 600, 285, 360 + vEhc.getV(0,0)*14, 3, 99999 * 10000).isV(vEhc,0,0).wrap(core.SummonSkillWrapper)    #보스 공격시 에너지 소모량 70% 감소, 틱당 에너지 85 소모.
         SerpentScrewDummy = core.SummonSkill("서펜트 스크류(지속)", 0, 1000, 0, 0, 99999 * 10000, cooltime = -1).wrap(core.SummonSkillWrapper)   #초당 에너지 60 소모 #보스 공격시 에너지 소모량 70% 감소, 틱당 에너지 85 소모.
@@ -127,7 +129,7 @@ class JobGenerator(ck.JobGenerator):
     
         FuriousCharge = core.DamageSkill("퓨리어스 차지", 540, 600+vEhc.getV(4,4)*24, 10, cooltime = 10 * 1000, modifier = core.CharacterModifier(boss_pdamage = 30)).isV(vEhc,4,4).wrap(core.DamageSkillWrapper)
         ######   Skill Wrapper   ######
-        EnergyCharge = EnergyChargeWrapper()
+        EnergyCharge = EnergyChargeWrapper(passive_level)
         EnergyCharge.set_name_style("게이지 %d만큼 변화")
         #1중첩 럭다 재사용 50초 감소 / 방어력30% / 체엠 20% / 크리율15% / 뎀증20 / 경치30
         #2중첩 럭다 재사용 50초 감소 / 방어력40% / 체엠 30% / 크리율25% / 뎀증30 / 경치40
