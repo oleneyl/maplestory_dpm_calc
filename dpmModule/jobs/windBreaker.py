225c7eb1054a83946b693c56647aa389d68aa059
cheese98 <cheesej98@gmail.com>
2020-09-01 11:23:30 +0900

    윈드브레이커 컴뱃오더스 적용

--- dpmModule/jobs/windBreaker.py
+++ dpmModule/jobs/windBreaker.py
@@ -6,6 +6,7 @@ from ..status.ability import Ability_tool
 from . import globalSkill
 from .jobbranch import bowmen
 from .jobclass import cygnus
+from math import ceil
 #TODO : 5차 신스킬 적용
 
 class JobGenerator(ck.JobGenerator):
@@ -16,21 +17,26 @@ class JobGenerator(ck.JobGenerator):
         self.jobname = "윈드브레이커"
         self.ability_list = Ability_tool.get_ability_set('boss_pdamage', 'crit', 'buff_rem')
         self._use_critical_reinforce = True
+        self._combat = 0
 
     def get_passive_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
+        passive_level = chtr.get_base_modifier().passive_level + self._combat
+
         ElementalExpert = core.InformedCharacterModifier("엘리멘탈 엑스퍼트", patt = 10)
         ElementalHarmony = core.InformedCharacterModifier("엘리멘탈 하모니", stat_main = chtr.level // 2)
         
         WhisperOfWind = core.InformedCharacterModifier("위스퍼 오브 윈드", att = 20)
         PhisicalTraining = core.InformedCharacterModifier("피지컬 트레이닝", stat_main = 30, stat_sub = 30)
         
-        WindBlessingPassive = core.InformedCharacterModifier("윈드 블레싱(패시브)", pstat_main = 15, patt = 10 + self.combat*1)
-        BowExpert = core.InformedCharacterModifier("보우 엑스퍼트", att = 30 + self.combat*1, crit_damage = 20, pdamage_indep = 25 + self.combat*1, boss_pdamage = 40 + self.combat*1)
+        WindBlessingPassive = core.InformedCharacterModifier("윈드 블레싱(패시브)", pstat_main = 15+passive_level//3, patt = 10 + ceil(passive_level/3))
+        BowExpert = core.InformedCharacterModifier("보우 엑스퍼트", att = 30 + passive_level, crit_damage = 20+passive_level//2, pdamage_indep = 25 + passive_level//3, boss_pdamage = 40 + passive_level)
         return [ElementalExpert, ElementalHarmony, WhisperOfWind, PhisicalTraining, BowExpert, WindBlessingPassive]
 
     def get_not_implied_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
+        passive_level = chtr.get_base_modifier().passive_level + self._combat
+
         WeaponConstant = core.InformedCharacterModifier("무기상수",pdamage_indep = 30)
-        Mastery = core.InformedCharacterModifier("숙련도",pdamage_indep = -7.5 + self.combat*0.5)
+        Mastery = core.InformedCharacterModifier("숙련도",pdamage_indep = -7.5 + 0.5*ceil(passive_level / 2))
         
         return [WeaponConstant, Mastery]
 
@@ -45,16 +51,17 @@ class JobGenerator(ck.JobGenerator):
         트라이플링 윔 평균치로 계산
         
         '''
+        passive_level = chtr.get_base_modifier().passive_level + self._combat
         #Buff skills
         Storm = core.BuffSkill("엘리멘트(스톰)", 0, 200 * 1000, pdamage = 10, rem = True).wrap(core.BuffSkillWrapper) #딜레이 모름
         SylphsAid = core.BuffSkill("실프스 에이드", 0, 200 * 1000, att = 20, crit = 10, rem = True).wrap(core.BuffSkillWrapper) #딜레이 모름
-        Albatross = core.BuffSkill("알바트로스 맥시멈", 0, 200 * 1000, att = 50 + combat*1, pdamage = 25, armor_ignore = 15, crit = 25, rem = True).wrap(core.BuffSkillWrapper)  #900 -> 690
-        SharpEyes = core.BuffSkill("샤프 아이즈", 660, 300 * 1000, crit = 20 + combat*1, crit_damage = 15 + combat*1, rem = True).wrap(core.BuffSkillWrapper)
+        Albatross = core.BuffSkill("알바트로스 맥시멈", 0, 200 * 1000, att = 50 + passive_level, pdamage = 25+2*(passive_level//3), armor_ignore = 15+passive_level//3, crit = 25+passive_level//2, rem = True).wrap(core.BuffSkillWrapper)  #900 -> 690
+        SharpEyes = core.BuffSkill("샤프 아이즈", 660, (300+10*self._combat) * 1000, crit = 20 + ceil(self._combat/2), crit_damage = 15 + ceil(self._combat/2), rem = True).wrap(core.BuffSkillWrapper)
         GloryOfGuardians = core.BuffSkill("글로리 오브 가디언즈", 0, 60 * 1000, cooltime = 120 * 1000, pdamage = 10).wrap(core.BuffSkillWrapper)
         
         StormBringerDummy = core.BuffSkill("스톰 브링어(버프)", 0, 200 * 1000).wrap(core.BuffSkillWrapper)  #딜레이 계산 필요
         # 하이퍼: 데미지 증가, 확률 10% 증가, 타수 증가
-        TriflingWhim = core.DamageSkill("트라이플링 윔", 0, (290 + combat*3) * 0.8 + (390 + combat*3) * 0.2, 2 * (0.5 + 0.1), modifier = core.CharacterModifier(pdamage = 20)).setV(vEhc, 1, 2, False).wrap(core.DamageSkillWrapper)
+        TriflingWhim = core.DamageSkill("트라이플링 윔", 0, (290 + passive_level*3) * 0.8 + (390 + passive_level*3) * 0.2, 2 * (0.5 + 0.1), modifier = core.CharacterModifier(pdamage = 20)).setV(vEhc, 1, 2, False).wrap(core.DamageSkillWrapper)
         StormBringer = core.DamageSkill("스톰 브링어", 0, 500, 0.3).setV(vEhc, 2, 2, True).wrap(core.DamageSkillWrapper)
     
         # 핀포인트 피어스
@@ -63,7 +70,7 @@ class JobGenerator(ck.JobGenerator):
 
         #Damage Skills
         # 하이퍼: 데미지 증가, 보스 데미지 증가
-        SongOfHeaven = core.DamageSkill("천공의 노래", 120, 345 + combat*3, 1, modifier = core.CharacterModifier(pdamage = ((1.2 + combat*0.01)**4 - 1) * 100 + 20, boss_pdamage = 30)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper) #코강렙 20이상 가정.
+        SongOfHeaven = core.DamageSkill("천공의 노래", 120, 345 + self._combat*3, 1, modifier = core.CharacterModifier(pdamage = ((1.2 + self._combat*0.01)**4 - 1) * 100 + 20, boss_pdamage = 30)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper) #코강렙 20이상 가정.
         
         CygnusPalanks = core.SummonSkill("시그너스 팔랑크스", 600, 120 * 5, 450 + 18*vEhc.getV(0,0), 5, 120 * (40 + vEhc.getV(0,0)), cooltime = 30 * 1000, red=True).isV(vEhc,0,0).wrap(core.SummonSkillWrapper)
         
@@ -89,7 +96,7 @@ class JobGenerator(ck.JobGenerator):
         Mercilesswind.onAfter(MercilesswindDOT)
 
         return(SongOfHeaven, 
-                [globalSkill.maple_heros(chtr.level, combat_level=combat), 
+                [globalSkill.maple_heros(chtr.level), 
                     Storm, SylphsAid, Albatross, SharpEyes, GloryOfGuardians, StormBringerDummy, CriticalReinforce,
                     PinPointPierceDebuff,
                     globalSkill.soul_contract()] +\
