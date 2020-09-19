d41308753fe65ffbec4145eadc299827150793f0
cheese98 <cheesej98@gmail.com>
2020-09-19 13:59:15 +0900

    1~4차, 하이퍼 스킬 등록

--- dpmModule/jobs/xenon.py
+++ dpmModule/jobs/xenon.py
@@ -8,6 +8,15 @@ from .jobbranch import thieves, pirates
 from .jobclass import resistance
 from math import ceil
 
+class SupplyStackWrapper(core.StackSkillWrapper):
+    def __init__(self, skill):
+        super(SupplyStackWrapper, self).__init__(skill, 20)
+        self.stack = 20
+        self.modifierInvariantFlag = False
+    
+    def get_modifier(self): # 서플러스 서플라이: 서플러스 에너지 1개 당 모든 능력치 1%만큼 증가
+        return core.CharacterModifier(pstat_main = self.stack, pstat_sub = self.stack)
+
 class JobGenerator(ck.JobGenerator):
     def __init__(self):
         super(JobGenerator, self).__init__()
@@ -23,6 +32,20 @@ class JobGenerator(ck.JobGenerator):
     def get_passive_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
         
+        Multilateral1 = core.InformedCharacterModifier("멀티래터럴 I", pdamage = 3)
+        Multilateral2 = core.InformedCharacterModifier("멀티래터럴 II", pdamage = 5)
+        Multilateral3 = core.InformedCharacterModifier("멀티래터럴 III", pdamage = 7)
+        Multilateral4 = core.InformedCharacterModifier("멀티래터럴 IV", pdamage = 10)
+        Multilateral5 = core.InformedCharacterModifier("멀티래터럴 V", pdamage = 10)
+        Multilateral6 = core.InformedCharacterModifier("멀티래터럴 V", pdamage = 5)
+
+        LinearPerspective = core.InformedCharacterModifier("리니어 퍼스펙티브", crit = 40)
+        MinoritySupport = core.InformedCharacterModifier("마이너리티 서포트", stat_main = 20, stat_sub = 20)
+        XenonMastery = core.InformedCharacterModifier("제논 마스터리", att = 20)
+        XenonExpert = core.InformedCharacterModifier("제논 엑스퍼트", att = 30, crit_damage = 8)
+        OffensiveMatrix = core.InformedCharacterModifier("오펜시브 매트릭스", armor_ignore = 30)
+
+
         return []
 
     def get_not_implied_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
@@ -43,12 +66,40 @@ class JobGenerator(ck.JobGenerator):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
         
         # Buff skills
+        SupplySurplus = core.BuffSkill("서플러스 서플라이", 0, 999999999).wrap(SupplyStackWrapper)
+        InclinePower = core.BuffSkill("인클라인 파워", None, 240000, att = 30).wrap(core.BuffSkillWrapper) # 에너지 -3
+        EfficiencyPipeLine = core.BuffSkill("에피션시 파이프라인", None, 240000).wrap(core.BuffSkillWrapper)
+        Booster = core.BuffSkill("제논 부스터", None, 240000).wrap(core.BuffSkillWrapper)
+        HybridDefenses = core.BuffSkill("듀얼브리드 디펜시브", None, 240000).wrap(core.BuffSkillWrapper) # 에너지 -7
+        VirtualProjection = core.BuffSkill("버추얼 프로젝션", None, 999999999).wrap(core.BuffSkillWrapper)
+
+        ExtraSupply = core.BuffSkill("엑스트라 서플라이", None, 1, cooltime = 30000).wrap(core.BuffSkillWrapper) # 에너지 +10
+
+        OOPArtsCode = core.BuffSkill("오파츠 코드", None, 30000, pdamage_indep = 25, boss_pdamage = 30).wrap(core.BuffSkillWrapper) # 에너지 -20
 
         # Damage skills
+
+        # 로켓강화 적용됨
+        PinpointRocket = core.DamageSkill("핀포인트 로켓", 0, 50+40+40+100, 4, cooltime = 2000).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
         
+        # 이지스: 피격시에만 발동이라 허수아비 상대로는 미적용해야할듯 (인스턴스 셔크 적용됨)
+        AegisSystem = core.DamageSkill("이지스 시스템", 0, 120, 3, modifier = core.CharacterModifier(pdamage = 20), cooltime = 1500).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
+        # 항상 3중첩 가정?
+        Triangulation = core.DamageSkill("트라이앵글 포메이션", 0, 340, 3, cooltime = -1).setV(vEhc, 0, 3, True).wrap(core.DamageSkillWrapper)
+
+        # 하이퍼 뎀퍼, 방무
+        PurgeSnipe = core.DamageSkill("퍼지롭 매스커레이드 : 저격", None, 345, 7, modifier = core.CharacterModifier(armor_ignore = 30) + core.CharacterModifier(pdamage = 20, armor_ignore = 10)).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
         
+        # 택1
+        # 하이퍼 3종 적용
+        Hologram_Penetrate = core.SummonSkill("홀로그램 그래피티 : 관통", None, None, 213*1.1, 1, 20000 + 10000, cooltime = 30000).setV(vEhc, 0, 2, True).wrap(core.SummonSkillWrapper)
+        Hologram_ForceField = core.SummonSkill("홀로그램 그래피티 : 역장", None, None, 400*1.1, 1, 20000 + 10000, cooltime = 30000).setV(vEhc, 0, 2, True).wrap(core.SummonSkillWrapper)
+
         # Hyper skills
-        
+        AmaranthGenerator = core.BuffSkill("아마란스 제네레이터", None, 10000, cooltime = 90000, rem = False).wrap(core.BuffSkillWrapper) # 에너지 최대치, 10초간 에너지 소모 없음
+        MeltDown = core.DamageSkill("멜트다운 익스플로전", None, 900, 6, red = False, cooltime = 50000).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
+        MeltDown_Armor = core.BuffSkill("멜트다운 익스플로전 (방무)", 0, 10000, armor_ignore = 30, rem = False).wrap(core.BuffSkillWrapper)
+        MeltDown_Damage = core.BuffSkill("멜트다운 익스플로전 (데미지)", 0, 25000, pdamage = 10, rem = False).wrap(core.BuffSkillWrapper)
     
         # V skills
         MirrorBreak, MirrorSpider = globalSkill.SpiderInMirrorBuilder(vEhc, 0, 0)
