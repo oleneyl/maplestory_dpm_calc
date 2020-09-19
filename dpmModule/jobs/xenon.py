5793a67211c136f2d4a38ebf45cfbbf24754b6d2
cheese98 <cheesej98@gmail.com>
2020-09-19 23:45:57 +0900

    컴뱃 적용, 쉐파 코드 추가

--- dpmModule/jobs/xenon.py
+++ dpmModule/jobs/xenon.py
@@ -32,25 +32,29 @@ class JobGenerator(ck.JobGenerator):
         
     def get_passive_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
-        
+        '''
+        효율성을 위해 합쳐진 코드 사용
         Multilateral1 = core.InformedCharacterModifier("멀티래터럴 I", pdamage = 3)
         Multilateral2 = core.InformedCharacterModifier("멀티래터럴 II", pdamage = 5)
         Multilateral3 = core.InformedCharacterModifier("멀티래터럴 III", pdamage = 7)
         Multilateral4 = core.InformedCharacterModifier("멀티래터럴 IV", pdamage = 10)
         Multilateral5 = core.InformedCharacterModifier("멀티래터럴 V", pdamage = 10)
-        Multilateral6 = core.InformedCharacterModifier("멀티래터럴 V", pdamage = 5)
+        Multilateral6 = core.InformedCharacterModifier("멀티래터럴 VI", pdamage = 5)
+        '''
+        Multilateral = core.InformedCharacterModifier("멀티래터럴", pdamage = 3+5+7+10+10+5)
 
         LinearPerspective = core.InformedCharacterModifier("리니어 퍼스펙티브", crit = 40)
         MinoritySupport = core.InformedCharacterModifier("마이너리티 서포트", stat_main = 20, stat_sub = 20)
         XenonMastery = core.InformedCharacterModifier("제논 마스터리", att = 20)
-        XenonExpert = core.InformedCharacterModifier("제논 엑스퍼트", att = 30, crit_damage = 8)
-        OffensiveMatrix = core.InformedCharacterModifier("오펜시브 매트릭스", armor_ignore = 30)
+        XenonExpert = core.InformedCharacterModifier("제논 엑스퍼트", att = 30 + passive_level, crit_damage = 8)
+        OffensiveMatrix = core.InformedCharacterModifier("오펜시브 매트릭스", armor_ignore = 30 + passive_level)
 
         LoadedDicePassive = pirates.LoadedDicePassiveWrapper(vEhc, 3, 4)
         ReadyToDiePassive = thieves.ReadyToDiePassiveWrapper(vEhc, 2, 2)
 
 
-        return []
+        return [Multilateral, LinearPerspective, MinoritySupport, XenonMastery, XenonExpert, OffensiveMatrix,
+        LoadedDicePassive, ReadyToDiePassive]
 
     def get_not_implied_skill_list(self, vEhc, chtr : ck.AbstractCharacter):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
@@ -79,7 +83,7 @@ class JobGenerator(ck.JobGenerator):
 
         ExtraSupply = core.BuffSkill("엑스트라 서플라이", None, 1, cooltime = 30000).wrap(core.BuffSkillWrapper) # 에너지 +10
 
-        OOPArtsCode = core.BuffSkill("오파츠 코드", None, 30000, pdamage_indep = 25, boss_pdamage = 30).wrap(core.BuffSkillWrapper) # 에너지 -20
+        OOPArtsCode = core.BuffSkill("오파츠 코드", None, (30+self.combat//2)*1000, pdamage_indep = 25 + self.combat//2, boss_pdamage = 30 + self.combat).wrap(core.BuffSkillWrapper) # 에너지 -20
 
         # Damage skills
 
@@ -87,17 +91,17 @@ class JobGenerator(ck.JobGenerator):
         PinpointRocket = core.DamageSkill("핀포인트 로켓", 0, 50+40+40+100, 4, cooltime = 2000).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
         
         # 이지스: 피격시에만 발동이라 허수아비 상대로는 미적용해야할듯 (인스턴스 셔크 적용됨)
-        AegisSystem = core.DamageSkill("이지스 시스템", 0, 120, 3, modifier = core.CharacterModifier(pdamage = 20), cooltime = 1500).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
+        AegisSystem = core.DamageSkill("이지스 시스템", 0, 120, 3, modifier = core.CharacterModifier(pdamage = 20 + passive_level // 3), cooltime = 1500).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
         # 항상 3중첩 가정?
         Triangulation = core.DamageSkill("트라이앵글 포메이션", 0, 340, 3, cooltime = -1).setV(vEhc, 0, 3, True).wrap(core.DamageSkillWrapper)
 
         # 하이퍼 뎀퍼, 방무
-        PurgeSnipe = core.DamageSkill("퍼지롭 매스커레이드 : 저격", None, 345, 7, modifier = core.CharacterModifier(armor_ignore = 30) + core.CharacterModifier(pdamage = 20, armor_ignore = 10)).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
+        PurgeSnipe = core.DamageSkill("퍼지롭 매스커레이드 : 저격", None, 345 + 2*self.combat, 7, modifier = core.CharacterModifier(armor_ignore = 30 + self.combat) + core.CharacterModifier(pdamage = 20, armor_ignore = 10)).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
         
         # 택1
         # 하이퍼 3종 적용
-        Hologram_Penetrate = core.SummonSkill("홀로그램 그래피티 : 관통", None, None, 213*1.1, 1, 20000 + 10000, cooltime = 30000).setV(vEhc, 0, 2, True).wrap(core.SummonSkillWrapper)
-        Hologram_ForceField = core.SummonSkill("홀로그램 그래피티 : 역장", None, None, 400*1.1, 1, 20000 + 10000, cooltime = 30000).setV(vEhc, 0, 2, True).wrap(core.SummonSkillWrapper)
+        Hologram_Penetrate = core.SummonSkill("홀로그램 그래피티 : 관통", None, None, (213+3*self.combat)*1.1, 1, 20000 + 10000, cooltime = 30000 - 1000 * ceil(self.combat/3)).setV(vEhc, 0, 2, True).wrap(core.SummonSkillWrapper)
+        Hologram_ForceField = core.SummonSkill("홀로그램 그래피티 : 역장", None, None, (400+5*self.combat)*1.1, 1, 20000 + 10000, cooltime = 30000 - 1000 * ceil(self.combat/3)).setV(vEhc, 0, 2, True).wrap(core.SummonSkillWrapper)
 
         # Hyper skills
         AmaranthGenerator = core.BuffSkill("아마란스 제네레이터", None, 10000, cooltime = 90000, rem = False).wrap(core.BuffSkillWrapper) # 에너지 최대치, 10초간 에너지 소모 없음
@@ -136,6 +140,10 @@ class JobGenerator(ck.JobGenerator):
         ######   Skill Wrapper   ######
 
         BasicAttackWrapper = core.DamageSkill("기본 공격", 0, 0, 0).wrap(core.DamageSkillWrapper)
+
+        # TODO: 쉐파 적용스킬 정확히 확인
+        for sk in [PurgeSnipe, MeltDown, MegaSmasherTick, PhotonRayTick]:
+            jobutils.create_auxilary_attack(sk, 0.7, nametag = "버추얼 프로젝션")
         
         return(BasicAttackWrapper, 
                 [globalSkill.maple_heros(chtr.level, combat_level=self.combat), globalSkill.useful_sharp_eyes(), globalSkill.useful_combat_orders(),
