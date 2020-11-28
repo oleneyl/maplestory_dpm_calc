f3d8d7e93846d2a19f5da743281c3e6c92bac6ba
cheese98 <cheesej98@gmail.com>
2020-11-28 12:39:36 +0900

    rules docstring 추가

--- dpmModule/execution/rules.py
+++ dpmModule/execution/rules.py
@@ -3,6 +3,9 @@ from collections import defaultdict
 from dpmModule.kernel.core import BuffSkillWrapper, DamageSkillWrapper, SummonSkillWrapper
 
 class ConditionRule(AbstractRule):
+    '''
+    두 GraphElement A,B와 check_function에 대해, check_function(B)가 True를 리턴하면 A를 사용.      
+    '''
     def __init__(self, state_element, checking_element, check_function):
         self._state_element_name = state_element
         self._checking_element_name = checking_element
@@ -15,6 +18,9 @@ class ConditionRule(AbstractRule):
         return self._check_function(reference_graph.get_element(self._checking_element_name))
 
 class UniquenessRule(AbstractRule):
+    '''
+    주어진 Element가 on 상태가 아니면 사용을 금지합니다. 이 Rule이 없다면, 버프나 소환수가 이미 켜져 있더라도, 계속 사용합니다.
+    '''
     def get_related_elements(self, reference_graph):
         return reference_graph.filter_elements(lambda x:isinstance(x, BuffSkillWrapper)) +\
             reference_graph.filter_elements(lambda x:isinstance(x, SummonSkillWrapper))
@@ -26,6 +32,9 @@ class UniquenessRule(AbstractRule):
             return True
 
 class ConcurrentRunRule(AbstractRule):
+    '''
+    두 GraphElement A,B에 대해, A가 B를 사용중일 때만 사용하도록 강제합니다.
+    '''
     def __init__(self, state_element, checking_element):
         self._state_element_name = state_element
         self._checking_element_name = checking_element
@@ -37,6 +46,9 @@ class ConcurrentRunRule(AbstractRule):
         return reference_graph.get_element(self._checking_element_name).is_active()
 
 class ReservationRule(AbstractRule):
+    '''
+    두 GraphElement A,B에 대해, A가 B가 사용가능할 때만 사용하도록 강제합니다.
+    '''
     def __init__(self, state_element, checking_element):
         self._state_element_name = state_element
         self._checking_element_name = checking_element
@@ -48,6 +60,11 @@ class ReservationRule(AbstractRule):
         return reference_graph.get_element(self._checking_element_name).is_usable()
 
 class SynchronizeRule(AbstractRule):
+    '''
+    B가 켜져있다면, B(버프 또는 소환수) 의 남은 시간이 `time(ms)` 이상(`direction`=1) / 이하(`direction`=-1) 일 때 A를 사용할 수 있습니다.
+
+    B가 꺼져있다면, A를 사용할 수 있습니다.
+    '''
     def __init__(self, target_element, timer_element, time, direction = 1):
         self._target_element = target_element
         self._timer_element = timer_element
@@ -66,6 +83,9 @@ class SynchronizeRule(AbstractRule):
         return True
 
 class MutualRule(AbstractRule):
+    '''
+    A를 B가 사용 가능할 때는 사용하지 않도록 합니다.
+    '''
     def __init__(self, target_element, state_element):
         self._target_element_name = target_element
         self._state_element_name = state_element
@@ -79,6 +99,9 @@ class MutualRule(AbstractRule):
         return False
 
 class InactiveRule(AbstractRule):
+    '''
+    A를 B가 사용되고 있을 때는 사용하지 않도록 합니다.
+    '''
     def __init__(self, target_element, state_element):
         self._target_element_name = target_element
         self._state_element_name = state_element
@@ -91,6 +114,9 @@ class InactiveRule(AbstractRule):
 
 
 class DisableRule(AbstractRule):
+    '''
+    주어진 GraphElement를 사용하지 못하도록 합니다.
+    '''
     def __init__(self, target_element):
         self.target_element = target_element
 
