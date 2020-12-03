66f9a6ad5dc8e223334c8e2f0171699cf3734d12
cheese98 <cheesej98@gmail.com>
2020-12-03 19:39:13 +0900

    globalSkill 코드 정리

--- dpmModule/jobs/globalSkill.py
+++ dpmModule/jobs/globalSkill.py
@@ -2,33 +2,19 @@ from dpmModule.character.characterKernel import AbstractCharacter
 from ..kernel import core
 import math
 
-#_SoulContract = core.BuffSkill("SoulContract", 600, 1000, cooltime = 9000, pdamage = 45)
-
 usefulSkillRemain = lambda slevel = 1: (180 + slevel * 3) * 1000
 passiveStat = lambda slevel = 1: (slevel + 4) // 5
 
-# 미완성 코드
-def useful_hyper_body_demonavenger(slevel = 1):
-    # 딜레이 클라기준
-    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디 (데몬어벤져)", 600, usefulSkillRemain(slevel), pstat_main = 40, stat_sub = passiveStat(slevel), rem = False).wrap(core.BuffSkillWrapper)
-    return UsefulHyperBody
-
-def useful_hyper_body_zenon(slevel = 1):
-    # 마나뻥 처리는 직업코드에서
-    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디 (제논)", 600, usefulSkillRemain(slevel), stat_main = passiveStat(slevel), stat_sub = passiveStat(slevel), rem = False).wrap(core.BuffSkillWrapper)
-    return UsefulHyperBody
-
-# 기본 90초, 직업별 딜사이클에 따라 쿨타임 조절가능
-def soul_contract(Cool = 90*1000):
-    SoulContract = core.BuffSkill("소울 컨트랙트", 900, 10*1000, cooltime = Cool, pdamage = 45, rem = True, red = True).wrap(core.BuffSkillWrapper)
+def soul_contract():
+    SoulContract = core.BuffSkill("소울 컨트랙트", 900, 10*1000, cooltime = 90000, pdamage = 45, rem = True, red = True).wrap(core.BuffSkillWrapper)
     return SoulContract
 
-# 쓸컴뱃 = 1, 팔라딘 = 2
+# 쓸컴뱃 = 1, 팔라딘 = 2, 제로 = 0
 def maple_heros(level, name = "메이플 용사", combat_level = 0):
     MapleHeros = core.BuffSkill(name, 0, (900+15*combat_level)*1000, stat_main = math.ceil(15 + 0.5 * combat_level)*0.01*(25 + level * 5), rem = True).wrap(core.BuffSkillWrapper)
     return MapleHeros
 
-# 1레벨
+# 1레벨 고정 (레벨당 보너스가 내성이므로)
 def useful_combat_orders():
     UsefulCombatOrders = core.BuffSkill("쓸만한 컴뱃 오더스", 1500, usefulSkillRemain(), rem = False).wrap(core.BuffSkillWrapper)
     return UsefulCombatOrders
@@ -101,3 +87,14 @@ def GenesisSkillBuilder(chtr: AbstractCharacter):
     AeonianRise = AeonianRiseWrapper(chtr)
 
     return TandadianRuin, AeonianRise
+
+# 미완성 코드
+def useful_hyper_body_demonavenger(slevel = 1):
+    # 딜레이 클라기준
+    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디 (데몬어벤져)", 600, usefulSkillRemain(slevel), pstat_main = 40, stat_sub = passiveStat(slevel), rem = False).wrap(core.BuffSkillWrapper)
+    return UsefulHyperBody
+
+def useful_hyper_body_xenon(slevel = 1):
+    # 마나뻥 처리는 직업코드에서
+    UsefulHyperBody = core.BuffSkill("쓸만한 하이퍼 바디 (제논)", 600, usefulSkillRemain(slevel), stat_main = passiveStat(slevel), stat_sub = passiveStat(slevel), rem = False).wrap(core.BuffSkillWrapper)
+    return UsefulHyperBody
