7bd35465cc45d940e77680f936f59c355e021105
oleneyl <39217532+oleneyl@users.noreply.github.com>
2020-12-03 20:59:52 +0900

    Fix to flake8

--- execute.py
+++ execute.py
@@ -1,7 +1,6 @@
-import dpmModule
 import dpmModule.character.characterTemplateHigh as template
 from dpmModule.util.dpmgenerator import IndividualDPMGenerator
 
 job_name = '나이트로드'
 gen = IndividualDPMGenerator(job_name, template.getU6000CharacterTemplate)
-print(job_name, gen.get_dpm(ulevel = 6000))
+print(job_name, gen.get_dpm(ulevel=6000))
