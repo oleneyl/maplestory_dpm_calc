cfc0d740795a77bfa89784de7f680c05f4b9998b
oleneyl <39217532+oleneyl@users.noreply.github.com>
2020-12-03 20:58:34 +0900

    Change execute.py

--- execute.py
+++ execute.py
@@ -1,6 +1,7 @@
 import dpmModule
-from dpmModule.util.dpmgenerator import IndividualDPMGenerator
 import dpmModule.character.characterTemplateHigh as template
+from dpmModule.util.dpmgenerator import IndividualDPMGenerator
+
 job_name = '나이트로드'
 gen = IndividualDPMGenerator(job_name, template.getU6000CharacterTemplate)
 print(job_name, gen.get_dpm(ulevel = 6000))
