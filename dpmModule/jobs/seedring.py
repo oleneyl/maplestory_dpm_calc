0eb65cdb9fbc1343fead1343db1af6a515efcfd1
cheese98 <cheesej98@gmail.com>
2021-01-16 11:29:01 +0900

    시드링 딜레이 추가

--- dpmModule/jobs/seedring.py
+++ dpmModule/jobs/seedring.py
@@ -1,10 +1,10 @@
 from ..kernel import core
 
-# TODO: 약칭을 사용 가능하도록 수정필요
-# 헬스컷, 마나컷, 리밋 등 효율이 낮거나 스위프트, 오버패스 등 딜 기여가 없는 링은 제외
 
 """
 시드링 (기본 4레벨)
+
+헬스컷, 마나컷, 리밋 등 효율이 낮거나 스위프트, 오버패스 등 딜 기여가 없는 링은 제외
 
 TODO: 링오브썸, 듀라빌리티, 얼티메이덤
 """
@@ -13,17 +13,18 @@ TODO: 링오브썸, 듀라빌리티, 얼티메이덤
 def restraint_ring(level: int = 4):
     return core.BuffSkill(
         "리스트레인트 링",
-        delay=0,
+        delay=60,
         remain=(7+2*level)*1000,
         cooltime=180000,
         patt=25*level,
         red=True
     ).wrap(core.BuffSkillWrapper)
 
+
 def crisis_hm_ring(level: int = 4):
     return core.BuffSkill(
         "크라이시스-HM링",
-        delay=0,
+        delay=60,
         remain=(7+2*level)*1000,
         cooltime=180000,
         patt=25*level,
@@ -34,7 +35,7 @@ def crisis_hm_ring(level: int = 4):
 def risktaker_ring(level: int = 4):
     return core.BuffSkill(
         "리스트테이커 링",
-        delay=0,
+        delay=60,
         remain=(6+6*level)*1000,
         cooltime=180000,
         patt=(level+1)*10,
@@ -45,7 +46,7 @@ def risktaker_ring(level: int = 4):
 def crisis_ring(level: int = 4):
     return core.BuffSkill(
         "크라이시스 H/M 링",
-        delay=0,
+        delay=60,
         remain=(7+2*level)*1000,
         cooltime=180000,
         patt=25*level,
@@ -58,7 +59,7 @@ def weaponpuff_ring(level: int = 4, weapon_att: int = -1):
         raise ValueError
     return core.BuffSkill(
         "웨폰퍼프 링",
-        delay=0,
+        delay=60,
         remain=(7+2*level)*1000,
         cooltime=180000,
         stat_main=level * weapon_att,
@@ -71,7 +72,7 @@ def levelpuff_ring(level: int = 4, chtr_level: int = -1):
         raise ValueError
     return core.BuffSkill(
         "레벨퍼프 링",
-        delay=0,
+        delay=60,
         remain=(7+2*level)*1000,
         cooltime=180000,
         stat_main=(2+7*level)*0.1*chtr_level,
@@ -82,7 +83,7 @@ def levelpuff_ring(level: int = 4, chtr_level: int = -1):
 def crit_damage_ring(level: int = 4):
     return core.BuffSkill(
         "크리데미지 링",
-        delay=0,
+        delay=60,
         remain=(7+2*level)*1000,
         cooltime=180000,
         crit_damage=7*level,
@@ -94,7 +95,7 @@ def crit_defense_ring(level: int = 4):
     # 크확 100% 가정
     return core.BuffSkill(
         "크리디펜스 링",
-        delay=0,
+        delay=60,
         remain=(7+2*level)*1000,
         cooltime=180000,
         armor_ignore=25*level,
