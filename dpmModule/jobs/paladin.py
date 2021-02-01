6c3735cac55c8909b525b360781793a9e3aae927
MrReds1324 <ryanb27@gmail.com>
2021-02-01 01:14:20 -0500

    Added english comments and doc strings to paladin.py

--- dpmModule/jobs/paladin.py
+++ dpmModule/jobs/paladin.py
@@ -8,7 +8,7 @@ from math import ceil
 from typing import Any, Dict
 
 
-# 4차 스킬은 컴뱃오더스 적용 기준으로 작성해야 함.
+# The 4th skill should be written based on Combat Orders. 4차 스킬은 컴뱃오더스 적용 기준으로 작성해야 함.
 class JobGenerator(ck.JobGenerator):
     def __init__(self):
         super(JobGenerator, self).__init__()
@@ -22,7 +22,7 @@ class JobGenerator(ck.JobGenerator):
         def use_soul_contract(soul_contract, grand_cross, holy_unity):
             if holy_unity.is_not_active():
                 return False
-            if grand_cross.is_usable() and holy_unity.is_time_left(9000, 1):  # 억지로 맞췄는데... 그크 직전에 정확히 쓰도록 해줄 방법이?
+            if grand_cross.is_usable() and holy_unity.is_time_left(9000, 1):  # I guessed it by force... Is there a way to make sure I use it right before it? 억지로 맞췄는데... 그크 직전에 정확히 쓰도록 해줄 방법이?
                 return True
             return False
 
@@ -60,15 +60,23 @@ class JobGenerator(ck.JobGenerator):
     def get_not_implied_skill_list(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
         passive_level = chtr.get_base_modifier().passive_level
         WeaponConstant = core.InformedCharacterModifier("무기상수", pdamage_indep=34)
-        Mastery = core.InformedCharacterModifier("숙련도", pdamage_indep=-4.5 + 0.5*ceil(passive_level/2))  # 오더스 기본적용!
+        Mastery = core.InformedCharacterModifier("숙련도", pdamage_indep=-4.5 + 0.5*ceil(passive_level/2))  # Basic application of orders! 오더스 기본적용!
 
-        ElementalCharge = core.InformedCharacterModifier("엘리멘탈 차지", pdamage=25, att=60)  # 조건부 적용 여부는 추후검토.
+        ElementalCharge = core.InformedCharacterModifier("엘리멘탈 차지", pdamage=25, att=60)  # The conditional application is reviewed later. 조건부 적용 여부는 추후검토.
         ParashockGuard = core.InformedCharacterModifier("파라쇼크 가드", att=20)
 
         return [WeaponConstant, Mastery, ElementalCharge, ParashockGuard]
 
     def generate(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
         '''
+        Two-handed blunt
+
+        Blae-Racha-Dicha-Saengchu-Pataek
+
+        Blessing armor not applied.
+
+        Blast-Reinforce, Bonus Attack / Sanctuary-Bonus Attack, Cooldown Reduce / Threat-Enhance
+
         두손둔기
 
         블래-라차-디차-생츄-파택
@@ -80,9 +88,9 @@ class JobGenerator(ck.JobGenerator):
         buff_rem = chtr.get_base_modifier().buff_rem
 
         # Buff skills
-        Threat = core.BuffSkill("위협", 1440, 80 * 1000, armor_ignore=30 + 20).wrap(core.BuffSkillWrapper)  # 기본 1080, 75% 확률 반영 딜레이
+        Threat = core.BuffSkill("위협", 1440, 80 * 1000, armor_ignore=30 + 20).wrap(core.BuffSkillWrapper)  # Default 1080, 75% probability reflected delay. 기본 1080, 75% 확률 반영 딜레이.
         # BlessingArmor = core.BuffSkill("블레싱 아머", 0, 30 * 1000, cooltime=90 * 1000, att=20, rem=True).wrap(core.BuffSkillWrapper)
-        ElementalForce = core.BuffSkill("엘리멘탈 포스", 0, 206 * 1000, pdamage_indep=21, rem=True).wrap(core.BuffSkillWrapper)  # 펫버프
+        ElementalForce = core.BuffSkill("엘리멘탈 포스", 0, 206 * 1000, pdamage_indep=21, rem=True).wrap(core.BuffSkillWrapper)  # Pet buff. 펫버프.
 
         EpicAdventure = core.BuffSkill("에픽 어드벤처", 0, 60*1000, cooltime=120 * 1000, pdamage=10).wrap(core.BuffSkillWrapper)
 
@@ -100,7 +108,7 @@ class JobGenerator(ck.JobGenerator):
         )
 
         # Damage Skills
-        LighteningCharge = core.DamageSkill("라이트닝 차지", 630, 462, 3+2, cooltime=60 * 1000 * (1 + buff_rem * 0.01)).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)  # 엘리멘탈 차지의 벞지 적용 고려함
+        LighteningCharge = core.DamageSkill("라이트닝 차지", 630, 462, 3+2, cooltime=60 * 1000 * (1 + buff_rem * 0.01)).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)  # Consider the potential application of elemental charge. 엘리멘탈 차지의 벞지 적용 고려함.
         LighteningChargeDOT = core.DotSkill("라이트닝 차지(도트)", 0, 1000, 115, 1, 6000, cooltime=-1).wrap(core.DotSkillWrapper)
         DivineCharge = core.DamageSkill("디바인 차지", 630, 462, 3+2, cooltime=60 * 1000 * (1 + buff_rem * 0.01)).setV(vEhc, 1, 2, False).wrap(core.DamageSkillWrapper)
         Sanctuary = core.DamageSkill("생츄어리", 750, 580, 8+2, cooltime=14 * 0.7 * 1000, red=True, modifier=core.CharacterModifier(boss_pdamage=30)).setV(vEhc, 3, 2, False).wrap(core.DamageSkillWrapper)
@@ -116,8 +124,8 @@ class JobGenerator(ck.JobGenerator):
         BlessedHammer = core.SummonSkill("블래스드 해머", 0, 600, 250 + vEhc.getV(1, 1)*10, 2, 999999 * 10000).isV(vEhc, 1, 1).wrap(core.SummonSkillWrapper)
         BlessedHammerActive = core.SummonSkill("블레스드 해머(활성화)", 360, 600, 525+vEhc.getV(1, 1)*21, 3, 30 * 1000, cooltime=60 * 1000, red=True).isV(vEhc, 1, 1).wrap(core.SummonSkillWrapper)
         GrandCross = core.DamageSkill("그랜드 크로스", 900, 0, 0, cooltime=150 * 1000, red=True).wrap(core.DamageSkillWrapper)
-        GrandCrossSmallTick = core.DamageSkill("그랜드 크로스(작은)", 200, 175 + vEhc.getV(3, 3)*7, 12, modifier=core.CharacterModifier(crit=100, armor_ignore=100)).isV(vEhc, 3, 3).wrap(core.DamageSkillWrapper)  # 3s, 15*6타
-        GrandCrossLargeTick = core.DamageSkill("그랜드 크로스(강화)", 146, 300 + vEhc.getV(3, 3)*12, 12, modifier=core.CharacterModifier(crit=100, armor_ignore=100)).isV(vEhc, 3, 3).wrap(core.DamageSkillWrapper)  # 6s, 41*6타
+        GrandCrossSmallTick = core.DamageSkill("그랜드 크로스(작은)", 200, 175 + vEhc.getV(3, 3)*7, 12, modifier=core.CharacterModifier(crit=100, armor_ignore=100)).isV(vEhc, 3, 3).wrap(core.DamageSkillWrapper)  # 3s, 15*6 strokes. 3s, 15*6타.
+        GrandCrossLargeTick = core.DamageSkill("그랜드 크로스(강화)", 146, 300 + vEhc.getV(3, 3)*12, 12, modifier=core.CharacterModifier(crit=100, armor_ignore=100)).isV(vEhc, 3, 3).wrap(core.DamageSkillWrapper)  # 6s, 41*6 strokes. 6s, 41*6타.
         GrandCrossEnd = core.DamageSkill("그랜드 크로스(종료)", 450, 0, 0).wrap(core.DamageSkillWrapper)
 
         FinalAttack = core.DamageSkill("파이널 어택", 0, 80, 2*0.4).setV(vEhc, 4, 5, True).wrap(core.DamageSkillWrapper)
@@ -144,7 +152,7 @@ class JobGenerator(ck.JobGenerator):
         MightyMjollnirInit.onAfter(core.RepeatElement(MightyMjollnir, 4))
         MightyMjollnir.onAfter(MightyMjollnirWave)
 
-        # 오라 웨폰
+        # Weapon Aura. 오라 웨폰.
         auraweapon_builder = warriors.AuraWeaponBuilder(vEhc, 2, 2)
         for sk in [Blast, Sanctuary, GrandCrossSmallTick, GrandCrossLargeTick, MightyMjollnirInit]:
             auraweapon_builder.add_aura_weapon(sk)
