5a8f3791199a0c3304131f1c8a55476163c0b06e
MrReds1324 <ryanb27@gmail.com>
2021-02-01 01:16:30 -0500

    Added english comments and doc strings to eunwol.py

--- dpmModule/jobs/eunwol.py
+++ dpmModule/jobs/eunwol.py
@@ -18,7 +18,7 @@ class SoulTrapStackWrapper(core.StackSkillWrapper):
         super(SoulTrapStackWrapper, self).__init__(skill, 10)
 
     def _add_debuff(self):
-        # 디버프 추가, 최대 _max(10)개로 유지
+        # Add debuff, keep up to _max(10). 디버프 추가, 최대 _max(10)개로 유지.
         self.debuffQueue = ([self.currentTime] + self.debuffQueue)[:self._max]
         self.stack = len(self.debuffQueue)
         return core.ResultObject(0, core.CharacterModifier(), 0, 0, '귀문진 디버프 변경', spec = 'graph control')
@@ -28,7 +28,7 @@ class SoulTrapStackWrapper(core.StackSkillWrapper):
 
     def spend_time(self, time):
         self.currentTime += time
-        # 지속시간이 끝난 디버프 제거
+        # Removes debuff over duration. 지속시간이 끝난 디버프 제거.
         self.debuffQueue = [x for x in self.debuffQueue if x + self.DEBUF_PERSISTENCE_TIME > self.currentTime]
         self.stack = len(self.debuffQueue)
         super(SoulTrapStackWrapper, self).spend_time(time)
@@ -75,9 +75,9 @@ class JobGenerator(ck.JobGenerator):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
         WeaponConstant = core.InformedCharacterModifier("무기상수",pdamage_indep = 70)
         Mastery = core.InformedCharacterModifier("숙련도", pdamage_indep = -5 + 0.5 * 2 * (passive_level // 3))   
-        Weakness = core.InformedCharacterModifier("약화",pdamage = 20) #디버프지만 상시발동가정
+        Weakness = core.InformedCharacterModifier("약화",pdamage = 20)  # It is debuff, but it is always activated. 디버프지만 상시발동가정.
 
-        # 약점 간파: 체력 (50 + passive_level)% 이하일 때 발동
+        # Weakness Detection: Triggers when HP is below (50 + passive_level)%. 약점 간파: 체력 (50 + passive_level)% 이하일 때 발동.
 
         WEAKNESS_BONUS = options.get("hp_rate", False)
 
@@ -87,6 +87,23 @@ class JobGenerator(ck.JobGenerator):
 
     def generate(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
         '''
+        ----Information---
+        Hyper: 3 returnees, reinforcement of the Explosive Zone, Fox Spirit summon rate +10%
+        Small Wedding Curtain 180ms
+        Attack every 1280ms
+
+        V Enhancement: (15)
+        Return, Explosive Zone, Fox Spirit
+        Separation, small marriage curtain
+
+        100% maintenance of cleanliness
+
+        Elemental Focus: Random skill activation once, keydown lasts 3 seconds,
+                   Elemental attack is activated every 2 seconds 1742 Perdem assumes one attack. Two fox spirits ejected
+        Small Spirit Veil: Use Ranghorn Veil every 60 seconds. If you can use Jin Gwicham, use it first and use the small wedding curtain
+        Guidance: Do not use wall cans
+        Separation Fear: Assumed as a mobile boss
+
         ----정보---
         하이퍼 : 귀참 3개, 폭류권 리인포스, 여우령 소환확률 +10%
         소혼장막 180ms
@@ -119,14 +136,14 @@ class JobGenerator(ck.JobGenerator):
         DoubleBody = core.BuffSkill("분혼 격참", 810, 10000, cooltime = (180-6*self.combat) * 1000, red = True, pdamage_indep = 20).wrap(core.BuffSkillWrapper)
         DoubleBodyRegistance = core.BuffSkill("분혼 격참(저항)", 0, 90000, cooltime = -1).wrap(core.BuffSkillWrapper)
 
-        #하이퍼스킬
-        #정결극 유지율 100%
+        # Hyperkill. 하이퍼스킬.
+        # 100% cleanliness retention rate. 정결극 유지율 100%.
         EnhanceSpiritLink = core.BuffSkill("정령 결속 극대화", 0, 120000 * (SOULENHANCEREM/100), cooltime = 120*1000, boss_pdamage = 20, pdamage = 35, att = 20, armor_ignore = 20).wrap(core.BuffSkillWrapper)
         EnhanceSpiritLinkSummon_S = core.SummonSkill("수호 정령(간간 수월래)", 0, 3000, 275, 3, 120000 * (SOULENHANCEREM/100), cooltime = -1).wrap(core.SummonSkillWrapper)
         EnhanceSpiritLinkSummon_J_Init = core.SummonSkill("수호 정령(소혼 장막)(시전)", 0, 60 * 1000, 0, 0, 120000 * (SOULENHANCEREM/100), cooltime = -1).wrap(core.SummonSkillWrapper)
         EnhanceSpiritLinkSummon_J = core.SummonSkill("수호 정령(소혼 장막)", 0, 150, 150, 1, 4800, cooltime = -1).wrap(core.SummonSkillWrapper)
 
-        # 랑과 무조건 함께 사용 (최종뎀 700%)
+        # Unconditionally used with Rang (700% of final damage). 랑과 무조건 함께 사용 (최종뎀 700%).
         SpiritFrenzy = core.DamageSkill("소혼 장막(시전)", 0, 0, 0, cooltime=10*1000 + 10080).wrap(core.DamageSkillWrapper)
         SpiritFrenzy_Tick = core.DamageSkill("소혼 장막", 180, 45, 5, cooltime = -1, modifier=core.CharacterModifier(pdamage_indep = 700)).setV(vEhc, 3, 3, False).wrap(core.DamageSkillWrapper)
 
@@ -140,23 +157,23 @@ class JobGenerator(ck.JobGenerator):
         SoulConcentrate = core.BuffSkill("정령 집속", 900, (30+vEhc.getV(2,1))*1000, cooltime = 120*1000, red=True, pdamage_indep = (5+vEhc.getV(2,1)//2)).isV(vEhc,2,1).wrap(core.BuffSkillWrapper)
         SoulConcentrateSummon = core.SummonSkill("정령 집속(무작위)", 0, 2000, 1742, 1, (30+vEhc.getV(2,1))*1000, cooltime = -1).isV(vEhc,2,1).wrap(core.SummonSkillWrapper)
 
-        # 귀문진: 정령을 한번에 2마리 소환함
+        # Gwimunjin: Summons 2 spirits at once. 귀문진: 정령을 한번에 2마리 소환함.
         SoulTrap = core.SummonSkill("귀문진", 990, 1280, 0, 3 * 2, 40000, cooltime = (120-vEhc.getV(3,2))*1000, red=True).isV(vEhc,3,2).wrap(core.SummonSkillWrapper)
         SoulTrap_D = core.DamageSkill("귀문진 공격", 0, 300+12*vEhc.getV(3,2), 6 * 2, cooltime = -1).isV(vEhc,3,2).wrap(core.DamageSkillWrapper)
 
         RealSoulAttack = core.DamageSkill("진 귀참", 540, 540+6*vEhc.getV(1,3), 12 + 1, cooltime=6000, red=True, modifier = core.CharacterModifier(pdamage = 20, boss_pdamage = 20) + core.CharacterModifier(armor_ignore=50)).setV(vEhc, 0, 2, False).isV(vEhc,1,3).wrap(core.DamageSkillWrapper)
 
         ChainBombPunchInit = core.DamageSkill("파쇄 연권(시전)", 390, 0, 0, cooltime=90*1000, red=True).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
-        ChainBombPunchTick = core.DamageSkill("파쇄 연권(키다운)", 930/8, 400+16*vEhc.getV(0,0), 5, cooltime=-1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper) # 5타씩 8회, 시전+키다운 1320ms
-        ChainBombPunchFinal = core.DamageSkill("파쇄 연권(막타)", 810, 950+38*vEhc.getV(0,0), 15*3, cooltime=-1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper) # 15타씩 3회
+        ChainBombPunchTick = core.DamageSkill("파쇄 연권(키다운)", 930/8, 400+16*vEhc.getV(0,0), 5, cooltime=-1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)  # 8 times of 5 strokes, cast + key down 1320ms. 5타씩 8회, 시전+키다운 1320ms.
+        ChainBombPunchFinal = core.DamageSkill("파쇄 연권(막타)", 810, 950+38*vEhc.getV(0,0), 15*3, cooltime=-1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)  # 3 times of 15 strokes. 15타씩 3회.
 
-        # 파쇄철조 (디버프용)
+        # Shredded iron (for debuff). 파쇄철조 (디버프용).
         BladeImp = core.DamageSkill("파쇄철조-회", 360, 160, 4).wrap(core.DamageSkillWrapper)
         BladeImpBuff = core.BuffSkill("파쇄철조-반", 0, 15 * 1000, cooltime=-1, pdamage_indep=10).wrap(core.BuffSkillWrapper)
 
         ######   Skill Wrapper   ######
 
-        #분혼 격참
+        # Furious marriage. 분혼 격참.
         DoubleBody.onAfter(DoubleBodyAttack)
         DoubleBody.onAfter(DoubleBodyRegistance)
         DoubleBodyConstraint = core.ConstraintElement("분혼 격참(저항)(제한)", DoubleBodyRegistance, DoubleBodyRegistance.is_not_active)
@@ -164,36 +181,36 @@ class JobGenerator(ck.JobGenerator):
 
         def double_body_single_target(doubleBody):
             if doubleBody.is_active():
-                # 분혼 도중 1타겟 스킬 데미지 감소, (100/120) * (0.5*1 + 0.5*0.2)
+                # 1 target skill damage reduction during separation, (100/120) * (0.5*1 + 0.5*0.2). 분혼 도중 1타겟 스킬 데미지 감소, (100/120) * (0.5*1 + 0.5*0.2).
                 return core.CharacterModifier(pdamage_indep=-40) - core.CharacterModifier(pdamage_indep=20)
             return core.CharacterModifier()
 
-        #정결극 위계
+        # The hierarchy of chastity. 정결극 위계.
         EnhanceSpiritLink.onAfters([EnhanceSpiritLinkSummon_S, EnhanceSpiritLinkSummon_J_Init])
         EnhanceSpiritLinkSummon_J_Init.onTick(EnhanceSpiritLinkSummon_J)
 
-        #정령 집속
+        # Elemental cluster. 정령 집속.
         SoulConcentrate.onAfter(DoubleBody.controller(1.0, 'reduce_cooltime_p'))
         SoulConcentrate.onAfter(SoulConcentrateSummon)
 
-        #귀문진
+        # Gwimunjin. 귀문진.
         SoulTrapStack = SoulTrapStackWrapper(core.BuffSkill("귀문진(버프)", 0, 9999999, cooltime = -1))
-        SoulTrap.onTick(core.RepeatElement(SoulTrapStack.add_debuff(), 2)) # 한 번에 디버프 두 개 생성
+        SoulTrap.onTick(core.RepeatElement(SoulTrapStack.add_debuff(), 2))  # Generates two debuffs at once. 한 번에 디버프 두 개 생성.
         SoulTrap.onTick(SoulTrap_D)
         SoulTrap_D.add_runtime_modifier(DoubleBody, double_body_single_target)
 
-        #진 귀참 알고리즘
+        # True returning algorithm. 진 귀참 알고리즘.
         BasicAttack = core.OptionalElement(RealSoulAttack.is_available, RealSoulAttack, SoulAttack, name = "진 귀참 발동 가능?")
         RealSoulAttack.protect_from_running()
 
         BasicAttackWrapper = core.DamageSkill('기본 공격',0,0,0).wrap(core.DamageSkillWrapper)
         BasicAttackWrapper.onAfter(BasicAttack)
 
-        #파쇄 연권
+        # Shredding concession. 파쇄 연권.
         ChainBombPunchInit.onAfter(core.RepeatElement(ChainBombPunchTick, 8))
         ChainBombPunchInit.onAfter(ChainBombPunchFinal)
 
-        #여우령
+        # Spirit Fox. 여우령.
         FoxSoul.add_runtime_modifier(DoubleBody, double_body_single_target)
         FoxSoul_100.add_runtime_modifier(DoubleBody, double_body_single_target)
         BasicAttack.onAfter(FoxSoul)
@@ -204,10 +221,10 @@ class JobGenerator(ck.JobGenerator):
         ChainBombPunchTick.onAfter(FoxSoul_100)
         ChainBombPunchFinal.onAfter(core.RepeatElement(FoxSoul_100, 3))
 
-        #파쇄철조
+        # Crushed iron. 파쇄철조.
         BladeImp.onAfter(BladeImpBuff)
 
-        #소혼장막
+        # ??. 소혼장막.
         SpiritFrenzy.onAfter(core.RepeatElement(SpiritFrenzy_Tick, 56))
         SpiritFrenzyConstraint = core.ConstraintElement("소혼 장막(제한)", EnhanceSpiritLinkSummon_J, EnhanceSpiritLinkSummon_J.is_active)
         SpiritFrenzy.onConstraint(SpiritFrenzyConstraint)
