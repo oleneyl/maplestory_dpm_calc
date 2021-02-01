00f4910b9c377016d98f171fe2740ca920372556
MrReds1324 <ryanb27@gmail.com>
2021-02-01 01:14:04 -0500

    Added english comments and doc strings to phantom.py

--- dpmModule/jobs/phantom.py
+++ dpmModule/jobs/phantom.py
@@ -55,6 +55,19 @@ class JobGenerator(ck.JobGenerator):
 
     def generate(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
         '''
+        Hyper:
+        Tempoka-Reinforce / Cool Reduce
+        Earld-Reinforce, Ignorguard
+
+        100% Joker
+
+        v Core: 14
+        Tier 1: Ultimate, Noir, Temwa
+        Tier 2: Talpan 4
+
+        Pet Buff: Play of Aria, Meyong, Croce
+        Tempest of Card not used
+
         하이퍼 :
         템오카 - 리인포스 / 쿨리듀스
         얼드 - 리인포스, 이그노어가드
@@ -72,27 +85,27 @@ class JobGenerator(ck.JobGenerator):
 
         ##### Steal skills #####
 
-        # 훔친 스킬은 공격력에 패널티가 있습니다.
+        # Stolen skills have a penalty in attack power. 훔친 스킬은 공격력에 패널티가 있습니다.
         STEALSKILL = core.CharacterModifier(pdamage_indep=100*((1.2 / 1.3)-1))
 
-        # 1차
+        # Primary. 1차.
         CardinalDischarge = core.DamageSkill("카디널 디스차지(탤팬1)", 210, 90, 4, modifier=STEALSKILL).setV(vEhc, 0, 7, True).wrap(core.DamageSkillWrapper)
 
-        # 2차
-        # 힐+마오팬보다 분노가 허수아비딜은 잘나옴
+        # Secondary. 2차.
+        # Rage is better than Hill + Mao Fan, and scarecrow deals are better. 힐+마오팬보다 분노가 허수아비딜은 잘나옴.
         Fury = core.BuffSkill("분노(탤팬2)", 0, 180000, rem=True, att=30).wrap(core.BuffSkillWrapper)
-        CardinalBlast = core.DamageSkill("카디널 블래스트(탤팬2)", 240, 200, 4, modifier=STEALSKILL).setV(vEhc, 1, 5, False).wrap(core.DamageSkillWrapper)  # 210~270ms 랜덤 (1.2.338 기준 측정), 최종뎀 단리적용
+        CardinalBlast = core.DamageSkill("카디널 블래스트(탤팬2)", 240, 200, 4, modifier=STEALSKILL).setV(vEhc, 1, 5, False).wrap(core.DamageSkillWrapper)  # 210~270ms random (measured based on 1.2.338), final dem isolated applied. 210~270ms 랜덤 (1.2.338 기준 측정), 최종뎀 단리적용.
         Heal = core.BuffSkill("힐(탤팬2)", 450, 2*1000, cooltime=10*1000, pdamage_indep=10).wrap(core.BuffSkillWrapper)
 
-        # 3차
+        # 3rd. 3차.
         CrossoverChain = core.BuffSkill("크로스 오버 체인(탤팬3)", 0, 180000, rem=True, pdamage_indep=20).wrap(core.BuffSkillWrapper)
-        ArrowFlatter = core.SummonSkill("애로우 플래터(탤팬3)", 600, 210, 85, 1, 30 * 1000, modifier=STEALSKILL).setV(vEhc, 4, 3, False).wrap(core.SummonSkillWrapper)  # 딜레이 모름
+        ArrowFlatter = core.SummonSkill("애로우 플래터(탤팬3)", 600, 210, 85, 1, 30 * 1000, modifier=STEALSKILL).setV(vEhc, 4, 3, False).wrap(core.SummonSkillWrapper)  # I don't know the delay. 딜레이 모름.
 
-        # 4차
+        # 4th. 4차.
         FinalCut = core.DamageSkill("파이널 컷(탤팬4)", 450, 2000 + 20 * self.combat, 1, modifier=STEALSKILL, cooltime=90000, red=True).setV(vEhc, 3, 2, True).wrap(core.DamageSkillWrapper)
         FinalCutBuff = core.BuffSkill("파이널 컷(탤팬4)(버프)", 0, 60000, cooltime=-1, rem=True, pdamage_indep=40 + self.combat).wrap(core.BuffSkillWrapper)
 
-        # 하이퍼
+        # Hyper. 하이퍼.
         BoolsEye = core.BuffSkill("불스아이(탤팬H)", 960, 30 * 1000, cooltime=180 * 1000, crit=20, crit_damage=10, armor_ignore=20, pdamage=20).wrap(core.BuffSkillWrapper)
         Preparation = core.BuffSkill("프리퍼레이션(탤팬H)", 900, 30 * 1000, cooltime=120 * 1000, att=50, boss_pdamage=20).wrap(core.BuffSkillWrapper)
 
@@ -100,7 +113,7 @@ class JobGenerator(ck.JobGenerator):
 
         # Buff skills
 
-        Booster = core.BuffSkill("부스터", 0, 240 * 1000, rem=True).wrap(core.BuffSkillWrapper)    #딜레이 모름
+        Booster = core.BuffSkill("부스터", 0, 240 * 1000, rem=True).wrap(core.BuffSkillWrapper)    # I don't know the delay. 딜레이 모름.
 
         MileAiguilles = core.DamageSkill("얼티밋 드라이브", 150, 125 + self.combat, 3, modifier=core.CharacterModifier(pdamage=20, armor_ignore=20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
 
@@ -117,23 +130,23 @@ class JobGenerator(ck.JobGenerator):
         ReadyToDie = thieves.ReadyToDieWrapper(vEhc, 3, 3)
         MirrorBreak, MirrorSpider = globalSkill.SpiderInMirrorBuilder(vEhc, 0, 0)
 
-        # 트와일라이트 미적용 상태
+        # # Twilight not applied. 트와일라이트 미적용 상태.
         # Twilight = core.BuffSkill("트와일라이트", 150, 15000, cooltime=15000, armor_ignore=20 + self.combat//2).wrap(core.BuffSkillWrapper)
         # TwilightHit = core.DamageSkill("트와일라이트(공격)", 540, 450+3*self.combat, 3, cooltime=-1).wrap(core.DamageSkillWrapper)
 
         JokerInit = core.DamageSkill("조커(시전)", 540, 0, 0, cooltime=150000, red=True).isV(vEhc, 4, 4).wrap(core.DamageSkillWrapper)
-        JokerDamage = core.DamageSkill("조커", 460, 240+9*vEhc.getV(4, 4), 30).isV(vEhc, 4, 4).wrap(core.DamageSkillWrapper)  # 14회 반복, 총 420타이므로 30타로 적용
+        JokerDamage = core.DamageSkill("조커", 460, 240+9*vEhc.getV(4, 4), 30).isV(vEhc, 4, 4).wrap(core.DamageSkillWrapper)  # 14 repetitions, a total of 420 strokes, so 30 strokes applied. 14회 반복, 총 420타이므로 30타로 적용.
         JokerBuff = core.BuffSkill("조커(버프)", 1230, 30000, cooltime=-1, pdamage_indep=(1 + (vEhc.getV(4, 4) - 1) // 5) * 2 / 5).isV(vEhc, 4, 4).wrap(core.BuffSkillWrapper)
 
         BlackJack = core.SummonSkill("블랙잭", 570, 250, 400+16*vEhc.getV(1, 1), 1, 5000-1, cooltime=15000, red=True).isV(vEhc, 1, 1).wrap(core.SummonSkillWrapper)
         BlackJackFinal = core.DamageSkill("블랙잭(최종)", 0, 600+24*vEhc.getV(1, 1), 12, cooltime=-1).isV(vEhc, 1, 1).wrap(core.DamageSkillWrapper)
 
         MarkOfPhantom = core.DamageSkill("마크 오브 팬텀", 690, 300+12*vEhc.getV(2, 2), 6 * 7, cooltime=30000, red=True).isV(vEhc, 2, 2).wrap(core.DamageSkillWrapper)
-        MarkOfPhantomEnd = core.DamageSkill("마크 오브 팬텀(최종)", 0, 485+19*vEhc.getV(2, 2), 15).isV(vEhc, 2, 2).wrap(core.DamageSkillWrapper)  # 2회 반복
+        MarkOfPhantomEnd = core.DamageSkill("마크 오브 팬텀(최종)", 0, 485+19*vEhc.getV(2, 2), 15).isV(vEhc, 2, 2).wrap(core.DamageSkillWrapper)  # Repeat 2 times. 2회 반복.
 
         LiftBreak = core.DamageSkill("리프트 브레이크", 750, 400+16*vEhc.getV(0, 0), 7*7, cooltime=30000, red=True).isV(vEhc, 0, 0).wrap(core.DamageSkillWrapper)
 
-        #### 그래프 빌드
+        #### Build the graph. 그래프 빌드.
 
         FinalCut.onAfter(CarteNoir)
         FinalCut.onAfter(FinalCutBuff)
@@ -149,10 +162,10 @@ class JobGenerator(ck.JobGenerator):
         CarteNoir.onAfter(FullStack)
         MileAiguilles.onAfter(CarteNoir)
 
-        TempestOfCardInit.onAfter(core.RepeatElement(TempestOfCard, 45))  # 최대 56, 쿨감 4초 기준 딜사이클 최적화를 위해 45로 변경
+        TempestOfCardInit.onAfter(core.RepeatElement(TempestOfCard, 45))  # Changed to 45 to optimize the deal cycle based on a maximum of 56 and a cool feeling of 4 seconds. 최대 56, 쿨감 4초 기준 딜사이클 최적화를 위해 45로 변경.
         TempestOfCard.onAfter(CarteNoir)
 
-        JokerDamage.onAfter(core.RepeatElement(CarteNoir, 10))  # 조커는 느와르를 발동하나, 조커 3타에 1번씩만 들어가므로 30 / 3 = 10
+        JokerDamage.onAfter(core.RepeatElement(CarteNoir, 10))  # The Joker activates noir, but it only goes once per 3 Joker strokes, so 30/3 = 10. 조커는 느와르를 발동하나, 조커 3타에 1번씩만 들어가므로 30 / 3 = 10.
 
         JokerDamages = core.RepeatElement(JokerDamage, 14)
         JokerInit.onAfter(JokerDamages)
@@ -191,7 +204,7 @@ class JobGenerator(ck.JobGenerator):
             Talent2 = None
             Inturrupt = core.DamageSkill("연계 취소 딜레이", 360-210, 0, 0).wrap(core.DamageSkillWrapper)
             for sk in interrupting_skills:
-                sk.onBefore(Inturrupt)  # 블디 연계 취소 딜레이. TODO: 연계 취소 딜레이를 시뮬레이터에 구현
+                sk.onBefore(Inturrupt)  # Bloody linkage cancellation delay. 블디 연계 취소 딜레이. TODO: Implement linkage cancellation delay in simulator. 연계 취소 딜레이를 시뮬레이터에 구현.
         else:
             raise ValueError(DEALCYCLE)
 
