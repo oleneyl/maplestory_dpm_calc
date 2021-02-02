1910915cf0f0929357e1849348cc373c62045be6
MrReds1324 <ryanb27@gmail.com>
2021-02-02 03:18:12 -0500

    Added enum for english and korean skill names for cygnus knights

--- dpmModule/jobs/jobclass/cygnus.py
+++ dpmModule/jobs/jobclass/cygnus.py
@@ -1,17 +1,26 @@
+from enum import Enum
+
 from ...kernel import core
 from ...kernel.core import VSkillModifier as V
 from ...kernel.core import CharacterModifier as MDF
 from ...character import characterKernel as ck
 from functools import partial
 
+
+class CygnusSkills(Enum):
+    EmpressCygnusBlessing = 'Empress Cygnus\'s Blessing | 여제 시그너스의 축복'  # Taken from https://maplestory.fandom.com/wiki/Empress_Cygnus%27s_Blessing
+    TranscendentCygnusBlessing = 'Transcendent Cygnus\'s Blessing | 초월자 시그너스의 축복'  # Taken form https://maplestory.fandom.com/wiki/Transcendent_Cygnus%27s_Blessing#KMS_1.2.326
+    PhalanxCharge = 'Phalanx Charge | 시그너스 팔랑크스'  # Taken from https://maplestory.fandom.com/wiki/Phalanx_Charge
+
+
 def PhalanxChargeWrapper(vEhc, num1, num2, hit_rate = 1):
-    PhalanxCharge = core.SummonSkill("시그너스 팔랑크스", 600, 120, 450 + 18*vEhc.getV(num1, num2), 1, 120 * (40 + vEhc.getV(num1, num2)) * hit_rate, cooltime = 30 * 1000, red=True).isV(vEhc, num1, num2).wrap(core.SummonSkillWrapper)
+    PhalanxCharge = core.SummonSkill(CygnusSkills.PhalanxCharge.value, 600, 120, 450 + 18*vEhc.getV(num1, num2), 1, 120 * (40 + vEhc.getV(num1, num2)) * hit_rate, cooltime = 30 * 1000, red=True).isV(vEhc, num1, num2).wrap(core.SummonSkillWrapper)
     return PhalanxCharge
 
 class CygnusBlessWrapper(core.BuffSkillWrapper):
-    # 코드 정리 필요
+    # Code cleanup required. 코드 정리 필요.
     def __init__(self, enhancer, skill_importance, enhance_importance, level, interval = 4):
-        skill = core.BuffSkill("초월자 시그너스의 축복" if level >= 245 else "여제 시그너스의 축복", 480, 45000, cooltime = 240*1000, red=True)
+        skill = core.BuffSkill(CygnusSkills.TranscendentCygnusBlessing.value if level >= 245 else CygnusSkills.EmpressCygnusBlessing.value, 480, 45000, cooltime = 240*1000, red=True)
         super(CygnusBlessWrapper, self).__init__(skill)
 
         self.isUpgraded = (level >= 245)
@@ -24,10 +33,10 @@ class CygnusBlessWrapper(core.BuffSkillWrapper):
         self.damage_point = 4 + (enhancer.getV(self.skill_importance, self.enhance_importance))//15
         self.damage_limit = 90
         
-        #interval: 데미지 증가 주기 (확인 필요)
+        # interval: damage increase period (requires confirmation). interval: 데미지 증가 주기 (확인 필요).
         self.interval = interval
         
-        # 업그레이드 상태일경우 스펙 증가치 적용
+        # In the case of upgrade, the specification increase is applied. 업그레이드 상태일경우 스펙 증가치 적용.
         if self.isUpgraded:
             self.damage_point += 2
             self.damage_limit += 30
