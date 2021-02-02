5e0bd76f28756f5efc076e5dcb237750c1dc10bb
MrReds1324 <ryanb27@gmail.com>
2021-02-02 03:19:21 -0500

    Added enum for english and korean skill names for nova

--- dpmModule/jobs/jobclass/nova.py
+++ dpmModule/jobs/jobclass/nova.py
@@ -1,11 +1,18 @@
+from enum import Enum
+
 from ...kernel import core
 
 
-# TODO: (40+0.5*스킬레벨)%, 소수점 아래로 버림. 확률로 재사용 대기시간 미적용, 최대 5회까지만 미적용 가능
+class NovaSkills(Enum):
+    GrandisGoddessBlessing = 'Grandis Goddess\'s Blessing | 그란디스 여신의 축복'  # Taken from https://maplestory.fandom.com/wiki/Grandis_Goddess%27s_Blessing
+    MightoftheNova = 'Might of the Nova | 판테온'  # Taken from https://maplestory.fandom.com/wiki/Might_of_the_Nova
+
+
+# TODO: (40+0.5*skill level)%, rounded down to the decimal point. Chances are, cooldown is not applied, and can only be applied up to 5 times. (40+0.5*스킬레벨)%, 소수점 아래로 버림. 확률로 재사용 대기시간 미적용, 최대 5회까지만 미적용 가능.
 class NovaGoddessBlessWrapper(core.BuffSkillWrapper):
     def __init__(self, vEhc, num1, num2, morph_gauge=None):
         skill = core.BuffSkill(
-            "그란디스 여신의 축복(노바)",
+            f"{NovaSkills.GrandisGoddessBlessing.value}(Nova | 노바)",
             delay=480,
             remain=40 * 1000,
             pdamage=5 + vEhc.getV(num1, num2),
@@ -30,7 +37,7 @@ class NovaGoddessBlessWrapper(core.BuffSkillWrapper):
 def PantheonWrapper(vEhc, num1: int, num2: int):
     Pantheon = (
         core.DamageSkill(
-            "판테온",
+            NovaSkills.MightoftheNova.value,
             delay=390,  # base delay 510, AS applied
             damage=2000 + 80 * vEhc.getV(num1, num2),
             hit=10,
