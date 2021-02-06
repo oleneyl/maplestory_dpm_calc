f14bce2ec2796c39f7976da1b9206ea90e2daa7d
MrReds1324 <ryanb27@gmail.com>
2021-02-06 12:21:38 -0500

    Replaced skill name usage with enum for angelic buster

--- dpmModule/jobs/angelicbuster.py
+++ dpmModule/jobs/angelicbuster.py
@@ -69,7 +69,7 @@ def getAffinityIV(duration):  # TODO: Real-time calculation like wild hunter ano
     timeDiv = 5000 - duration * count
     prob = timeDiv / duration
     ratio = (1 - prob) * (1 - 0.5 ** count) + prob * (1 - 0.5 ** (count + 1))
-    return core.InformedCharacterModifier("어피니티 IV", pdamage=30 * ratio)
+    return core.InformedCharacterModifier(AngelicBusterSkills.AffinityHeartIV.value, pdamage=30 * ratio)
 
 
 class JobGenerator(ck.JobGenerator):
@@ -84,22 +84,22 @@ class JobGenerator(ck.JobGenerator):
 
     def get_ruleset(self):
         ruleset = RuleSet()
-        ruleset.add_rule(ConcurrentRunRule('소울 컨트랙트', '스포트라이트'), RuleSet.BASE)
+        ruleset.add_rule(ConcurrentRunRule(GlobalSkills.TermsAndConditions.value, AngelicBusterSkills.SuperstarSpotlight.value), RuleSet.BASE)
         return ruleset
 
     def get_passive_skill_list(self, vEhc, chtr: ck.AbstractCharacter, options: Dict[str, Any]):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
-        SoulShooterMastery = core.InformedCharacterModifier("소울슈터 마스터리", att=20)
-        InnerFire = core.InformedCharacterModifier("이너 파이어", stat_main=40)
+        SoulShooterMastery = core.InformedCharacterModifier(AngelicBusterSkills.SoulShooterMastery.value, att=20)
+        InnerFire = core.InformedCharacterModifier(AngelicBusterSkills.BeautifulSoul.value, stat_main=40)
 
-        CallOfAncient = core.InformedCharacterModifier("콜 오브 에인션트", att=40)
-        AffinityIII = core.InformedCharacterModifier("어피니티 III", stat_main=40, pdamage=20)
+        CallOfAncient = core.InformedCharacterModifier(AngelicBusterSkills.DragonWhistle.value, att=40)
+        AffinityIII = core.InformedCharacterModifier(AngelicBusterSkills.AffinityHeartIII.value, stat_main=40, pdamage=20)
         AffinityIV = getAffinityIV(1272.08)  # Should be changed every time the average period of trinity changes. 1000 * time(sec) / (Trinity usage count). 트리니티 평균 주기가 바뀔 때 마다 변경해 줘야함. 1000 * time(초) / (트리니티 사용 횟수).
-        TrinityPassive = core.InformedCharacterModifier("트리니티(패시브)", pdamage_indep=ceil((30 + self.combat) / 3), armor_ignore=ceil((30 + self.combat) / 2))
-        SoulShooterExpert = core.InformedCharacterModifier("소울슈터 엑스퍼트", att=30 + passive_level, crit=30 + passive_level, crit_damage=15 + ceil(passive_level / 2))
+        TrinityPassive = core.InformedCharacterModifier(f"{AngelicBusterSkills.Trinity.value}(passive | 패시브)", pdamage_indep=ceil((30 + self.combat) / 3), armor_ignore=ceil((30 + self.combat) / 2))
+        SoulShooterExpert = core.InformedCharacterModifier(AngelicBusterSkills.SoulShooterExpert.value, att=30 + passive_level, crit=30 + passive_level, crit_damage=15 + ceil(passive_level / 2))
 
         LoadedDicePassive = pirates.LoadedDicePassiveWrapper(vEhc, 1, 2)
-        TrinityFusionPassive = core.InformedCharacterModifier("트리니티 퓨전(패시브)", stat_main=10 + vEhc.getV(0, 0))
+        TrinityFusionPassive = core.InformedCharacterModifier(f"{AngelicBusterSkills.TrinityFusion.value}(passive | 패시브)", stat_main=10 + vEhc.getV(0, 0))
 
         return [SoulShooterMastery, InnerFire,
                 CallOfAncient, AffinityIII, AffinityIV, TrinityPassive, SoulShooterExpert,
@@ -165,31 +165,31 @@ class JobGenerator(ck.JobGenerator):
         TRINITY_MDF = core.CharacterModifier(pdamage=20) + core.CharacterModifier(pdamage=10 * 3, armor_ignore=10 * 3)  # 하이퍼 리인포스 + 3중첩
 
         # Buff skills
-        Booster = core.BuffSkill("리리컬 크로스", 0, 200 * 1000).wrap(core.BuffSkillWrapper)
+        Booster = core.BuffSkill(AngelicBusterSkills.MelodyCross.value, 0, 200 * 1000).wrap(core.BuffSkillWrapper)
 
-        SoulContract = core.BuffSkill("소울 컨트랙트", 600, 10000, rem=True, red=True, cooltime=90000, pdamage=90).wrap(core.BuffSkillWrapper)
-        SoulSeekerExpert = core.DamageSkill("소울 시커", 0, 320 * 0.75, 1 * 0.01 * (35 + self.combat) * 12.066, modifier=core.CharacterModifier(pdamage=20)).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
-        SoulSeekerExpert_PR = core.DamageSkill("소울 시커(소울 익절트)", 0, 320 * 0.75, 1 * 0.01 * (50 + self.combat) * 12.066,modifier=core.CharacterModifier(pdamage=20)).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
+        SoulContract = core.BuffSkill(GlobalSkills.TermsAndConditions.value, 600, 10000, rem=True, red=True, cooltime=90000, pdamage=90).wrap(core.BuffSkillWrapper)
+        SoulSeekerExpert = core.DamageSkill(AngelicBusterSkills.SoulSeekerExpert.value, 0, 320 * 0.75, 1 * 0.01 * (35 + self.combat) * 12.066, modifier=core.CharacterModifier(pdamage=20)).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
+        SoulSeekerExpert_PR = core.DamageSkill(f"{AngelicBusterSkills.SoulSeekerExpert.value}({AngelicBusterSkills.PrettyExaltation.value})", 0, 320 * 0.75, 1 * 0.01 * (50 + self.combat) * 12.066,modifier=core.CharacterModifier(pdamage=20)).setV(vEhc, 1, 2, True).wrap(core.DamageSkillWrapper)
 
         # -70 is a split attack. -70은 스플릿 어택.
         TRINITY_DAMAGE = 360 + 12 * (30 + self.combat) - 70
-        Trinity_1 = core.DamageSkill("트리니티", 360, TRINITY_DAMAGE, 2 + 1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
-        Trinity_2 = core.DamageSkill("트리니티(2타)", 360, TRINITY_DAMAGE, 3 + 1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
-        Trinity_3 = core.DamageSkill("트리니티(3타)", 360, TRINITY_DAMAGE, 4 + 1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
+        Trinity_1 = core.DamageSkill(AngelicBusterSkills.Trinity.value, 360, TRINITY_DAMAGE, 2 + 1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
+        Trinity_2 = core.DamageSkill(f"{AngelicBusterSkills.Trinity.value}(2nd hit | 2타)", 360, TRINITY_DAMAGE, 3 + 1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
+        Trinity_3 = core.DamageSkill(f"{AngelicBusterSkills.Trinity.value}(3rd hit | 3타)", 360, TRINITY_DAMAGE, 4 + 1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).wrap(core.DamageSkillWrapper)
 
-        FinaturaFettuccia = core.DamageSkill("피니투라 페투치아", 1020, 400 + 7 * self.combat, 10, red=True, cooltime=40000 * 0.75).setV(vEhc, 3, 2, False).wrap(
+        FinaturaFettuccia = core.DamageSkill(AngelicBusterSkills.FinaleRibbon.value, 1020, 400 + 7 * self.combat, 10, red=True, cooltime=40000 * 0.75).setV(vEhc, 3, 2, False).wrap(
             core.DamageSkillWrapper)
-        FinaturaFettucciaBuff = core.BuffSkill("피니투라 페투치아(버프)", 0, 20000, cooltime=-1, pdamage_indep=25).wrap(core.BuffSkillWrapper)
+        FinaturaFettucciaBuff = core.BuffSkill(f"{AngelicBusterSkills.FinaleRibbon.value}(buff | 버프)", 0, 20000, cooltime=-1, pdamage_indep=25).wrap(core.BuffSkillWrapper)
 
-        SoulGaze = core.BuffSkill("소울 게이즈", 1080, (180 + 5 * self.combat) * 1000, rem=True, crit_damage=45 + self.combat).wrap(core.BuffSkillWrapper)
+        SoulGaze = core.BuffSkill(AngelicBusterSkills.StarGazer.value, 1080, (180 + 5 * self.combat) * 1000, rem=True, crit_damage=45 + self.combat).wrap(core.BuffSkillWrapper)
 
-        # 하이퍼
-        SoulExult = core.BuffSkill("소울 익절트", 1020, 30000, armor_ignore=30, boss_pdamage=20, cooltime=120 * 1000).wrap(core.BuffSkillWrapper)
-        SuperNova = core.SummonSkill("슈퍼 노바", 600, 840, 600, 3, 12000, cooltime=60 * 1000).setV(vEhc, 2, 2, True).wrap(core.SummonSkillWrapper)  # 840ms 타격(14타)
-        FinalContract = core.BuffSkill("파이널 컨트랙트", 0, 30000, cooltime=120 * 1000, att=50, crit=30).wrap(core.BuffSkillWrapper)
+        # Hypers. 하이퍼.
+        SoulExult = core.BuffSkill(AngelicBusterSkills.PrettyExaltation.value, 1020, 30000, armor_ignore=30, boss_pdamage=20, cooltime=120 * 1000).wrap(core.BuffSkillWrapper)
+        SuperNova = core.SummonSkill(AngelicBusterSkills.SupremeSupernova.value, 600, 840, 600, 3, 12000, cooltime=60 * 1000).setV(vEhc, 2, 2, True).wrap(core.SummonSkillWrapper)  # 840ms 타격(14타)
+        FinalContract = core.BuffSkill(AngelicBusterSkills.FinalContract.value, 0, 30000, cooltime=120 * 1000, att=50, crit=30).wrap(core.BuffSkillWrapper)
 
         # 로디드 데미지 고정.
-        LuckyDice = core.BuffSkill("로디드 다이스", 0, 180 * 1000, pdamage=20).isV(vEhc, 1, 2).wrap(core.BuffSkillWrapper)
+        LuckyDice = core.BuffSkill(PirateSkills.LoadedDice.value, 0, 180 * 1000, pdamage=20).isV(vEhc, 1, 2).wrap(core.BuffSkillWrapper)
 
         WEAPON_ATT = jobutils.get_weapon_att(chtr)
         Overdrive = pirates.OverdriveWrapper(vEhc, 3, 3, WEAPON_ATT)
@@ -197,17 +197,17 @@ class JobGenerator(ck.JobGenerator):
         NovaGoddessBless = nova.NovaGoddessBlessWrapper(vEhc, 0, 0)
         Phanteon = nova.PantheonWrapper(vEhc, 0, 0)
 
-        EnergyBurst = core.DamageSkill("에너지 버스트", 900, (450 + 18 * vEhc.getV(4, 4)) * 3, 15, red=True, cooltime=120 * 1000).isV(vEhc, 4, 4).wrap(core.DamageSkillWrapper)
-        SpotLight = core.SummonSkill("스포트라이트", 990, 800, 400 + 16 * vEhc.getV(0, 0), 3 * SPOTLIGHTHIT, 30000, cooltime=120 * 1000, red=True).isV(vEhc, 0,0).wrap(core.SummonSkillWrapper)
-        SpotLightBuff = core.BuffSkill("스포트라이트(버프)", 0, 30000, cooltime=-1, crit=(10 + int(0.2 * vEhc.getV(0, 0))) * SPOTLIGHTHIT, pdamage_indep=(3 + (vEhc.getV(0, 0) // 10)) * SPOTLIGHTHIT).isV(vEhc, 0, 0).wrap(core.BuffSkillWrapper)
+        EnergyBurst = core.DamageSkill(AngelicBusterSkills.SparkleBurst.value, 900, (450 + 18 * vEhc.getV(4, 4)) * 3, 15, red=True, cooltime=120 * 1000).isV(vEhc, 4, 4).wrap(core.DamageSkillWrapper)
+        SpotLight = core.SummonSkill(AngelicBusterSkills.SuperstarSpotlight.value, 990, 800, 400 + 16 * vEhc.getV(0, 0), 3 * SPOTLIGHTHIT, 30000, cooltime=120 * 1000, red=True).isV(vEhc, 0,0).wrap(core.SummonSkillWrapper)
+        SpotLightBuff = core.BuffSkill(f"{AngelicBusterSkills.SuperstarSpotlight.value}(buff | 버프)", 0, 30000, cooltime=-1, crit=(10 + int(0.2 * vEhc.getV(0, 0))) * SPOTLIGHTHIT, pdamage_indep=(3 + (vEhc.getV(0, 0) // 10)) * SPOTLIGHTHIT).isV(vEhc, 0, 0).wrap(core.BuffSkillWrapper)
 
-        MascortFamilier = core.BuffSkill("마스코트 패밀리어", 810, 30 + (vEhc.getV(2, 1) // 5) * 1000, red=True, cooltime=120 * 1000).isV(vEhc, 2, 1).wrap(core.BuffSkillWrapper)
-        MascortFamilierAttack = core.SummonSkill("트윙클 스타/매지컬 벌룬", 0, 2500, 1200, 5, (30 + (vEhc.getV(2, 1) // 5)) * 1000, cooltime=-1).isV(vEhc, 2, 1).wrap(core.SummonSkillWrapper)
-        ShinyBubbleBreath = core.SummonSkill("샤이니 버블 브레스", 0, 210, 250 + 10 * vEhc.getV(2, 1), 7, (3 + 0.4 * 8) * 1000, cooltime=-1).isV(vEhc, 2, 1).wrap(core.SummonSkillWrapper)
+        MascortFamilier = core.BuffSkill(AngelicBusterSkills.MightyMascot.value, 810, 30 + (vEhc.getV(2, 1) // 5) * 1000, red=True, cooltime=120 * 1000).isV(vEhc, 2, 1).wrap(core.BuffSkillWrapper)
+        MascortFamilierAttack = core.SummonSkill("Twinkle Star/Magical Balloon | 트윙클 스타/매지컬 벌룬", 0, 2500, 1200, 5, (30 + (vEhc.getV(2, 1) // 5)) * 1000, cooltime=-1).isV(vEhc, 2, 1).wrap(core.SummonSkillWrapper)
+        ShinyBubbleBreath = core.SummonSkill("Shiny Bubble Breath | 샤이니 버블 브레스", 0, 210, 250 + 10 * vEhc.getV(2, 1), 7, (3 + 0.4 * 8) * 1000, cooltime=-1).isV(vEhc, 2, 1).wrap(core.SummonSkillWrapper)
 
         # Canceled the previous trinity delay by 150ms. 이전 트리니티 딜레이를 150ms만큼 캔슬함. TODO: Make trinity (cancellation) and fusion 660ms. 트리니티(캔슬) 만들고 퓨전 660ms로 할것
-        TrinityFusionInit = core.DamageSkill("트리니티 퓨전(시전)", 660 - 150, 0, 0, cooltime=(16 - vEhc.getV(0, 0) // 10) * 1000, red=True).isV(vEhc, 0, 0).wrap(core.DamageSkillWrapper)
-        TrinityFusion = core.DamageSkill("트리니티 퓨전", 0, 330 + vEhc.getV(0, 0), 3, cooltime=-1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).isV(vEhc, 0, 0).wrap(core.DamageSkillWrapper)
+        TrinityFusionInit = core.DamageSkill(f"{AngelicBusterSkills.TrinityFusion.value}(Cast | 시전)", 660 - 150, 0, 0, cooltime=(16 - vEhc.getV(0, 0) // 10) * 1000, red=True).isV(vEhc, 0, 0).wrap(core.DamageSkillWrapper)
+        TrinityFusion = core.DamageSkill(AngelicBusterSkills.TrinityFusion.value, 0, 330 + vEhc.getV(0, 0), 3, cooltime=-1, modifier=TRINITY_MDF).setV(vEhc, 0, 2, True).isV(vEhc, 0, 0).wrap(core.DamageSkillWrapper)
 
         ### build graph relationships
         Trinity_1.onAfter(Trinity_2)
@@ -236,7 +236,7 @@ class JobGenerator(ck.JobGenerator):
         return (
             Trinity_1,
             [
-                globalSkill.maple_heros(chtr.level, name="노바의 용사", combat_level=self.combat),
+                globalSkill.maple_heros(chtr.level, name=AngelicBusterSkills.NovaWarrior.value, combat_level=self.combat),
                 globalSkill.useful_sharp_eyes(),
                 globalSkill.useful_combat_orders(),
                 globalSkill.useful_wind_booster(),
