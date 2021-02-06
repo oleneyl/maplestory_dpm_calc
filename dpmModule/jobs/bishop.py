d51f91d84fa2d7386b1e64e858f334f140afb0d1
MrReds1324 <ryanb27@gmail.com>
2021-02-06 12:25:55 -0500

    Replaced skill name usage with enum for bishop

--- dpmModule/jobs/bishop.py
+++ dpmModule/jobs/bishop.py
@@ -68,7 +68,7 @@ class BishopSkills(Enum):
 
 class PrayWrapper(core.BuffSkillWrapper):
     def __init__(self, vEhc, num1, num2):
-        super(PrayWrapper, self).__init__(core.BuffSkill("프레이", 360, 1000 * (30 + vEhc.getV(num1,num2) // 2), cooltime = 180 * 1000, red = True).isV(vEhc, num1, num2))
+        super(PrayWrapper, self).__init__(core.BuffSkill(BishopSkills.Benediction.value, 360, 1000 * (30 + vEhc.getV(num1,num2) // 2), cooltime = 180 * 1000, red = True).isV(vEhc, num1, num2))
         self.enable_referring_runtime_context()
         self.stat = None
         self.modifierInvariantFlag = False
@@ -95,10 +95,10 @@ class JobGenerator(ck.JobGenerator):
 
     def get_ruleset(self):
         ruleset = RuleSet()
-        ruleset.add_rule(SynchronizeRule('소울 컨트랙트', '인피니티', 35000, -1), RuleSet.BASE)
-        ruleset.add_rule(SynchronizeRule('프레이', '인피니티', 45000, -1), RuleSet.BASE)
-        ruleset.add_rule(InactiveRule('언스테이블 메모라이즈', '인피니티'), RuleSet.BASE)
-        ruleset.add_rule(DisableRule('힐'), RuleSet.BASE)
+        ruleset.add_rule(SynchronizeRule('소울 컨트랙트', AdventurerSkills.Infinity.value, 35000, -1), RuleSet.BASE)
+        ruleset.add_rule(SynchronizeRule(BishopSkills.Benediction.value, AdventurerSkills.Infinity.value, 45000, -1), RuleSet.BASE)
+        ruleset.add_rule(InactiveRule(AdventurerSkills.UnreliableMemory.value, AdventurerSkills.Infinity.value), RuleSet.BASE)
+        ruleset.add_rule(DisableRule(BishopSkills.Heal.value), RuleSet.BASE)
         return ruleset
 
     def get_modifier_optimization_hint(self):
@@ -107,16 +107,16 @@ class JobGenerator(ck.JobGenerator):
     def get_passive_skill_list(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
 
-        HighWisdom = core.InformedCharacterModifier("하이 위즈덤",stat_main = 40)
-        SpellMastery = core.InformedCharacterModifier("스펠 마스터리",att = 10)
+        HighWisdom = core.InformedCharacterModifier(BishopSkills.HighWisdom.value,stat_main = 40)
+        SpellMastery = core.InformedCharacterModifier(BishopSkills.SpellMastery.value,att = 10)
         
-        MagicCritical = core.InformedCharacterModifier("매직 크리티컬",crit = 30, crit_damage = 13)
-        HolyFocus = core.InformedCharacterModifier("홀리 포커스",crit = 40)
+        MagicCritical = core.InformedCharacterModifier(BishopSkills.ArcaneOverdrive.value,crit = 30, crit_damage = 13)
+        HolyFocus = core.InformedCharacterModifier(BishopSkills.HolyFocus.value,crit = 40)
         
-        MasterMagic = core.InformedCharacterModifier("마스터 매직",att = 30 + passive_level * 3, buff_rem = 50 + passive_level * 5)
-        ArcaneAim = core.InformedCharacterModifier("아케인 에임", armor_ignore = 20)
+        MasterMagic = core.InformedCharacterModifier(BishopSkills.BuffMastery.value,att = 30 + passive_level * 3, buff_rem = 50 + passive_level * 5)
+        ArcaneAim = core.InformedCharacterModifier(BishopSkills.ArcaneAim.value, armor_ignore = 20)
         
-        VengenceOfAngelOff = core.InformedCharacterModifier("벤전스 오브 엔젤(off)",pdamage = 40)
+        VengenceOfAngelOff = core.InformedCharacterModifier(f"{BishopSkills.RighteouslyIndignant.value}(off)",pdamage = 40)
 
         UnstableMemorizePassive = adventurer.UnstableMemorizePassiveWrapper(vEhc, 4, 4)
         
@@ -127,11 +127,11 @@ class JobGenerator(ck.JobGenerator):
 
         WeaponConstant = core.InformedCharacterModifier("무기상수",pdamage_indep = 20)
         Mastery = core.InformedCharacterModifier("숙련도",pdamage_indep = -2.5)       
-        BlessingEnsemble = core.InformedCharacterModifier("블레싱 앙상블",pdamage_indep = 3)
+        BlessingEnsemble = core.InformedCharacterModifier(BishopSkills.BlessedEnsemble.value,pdamage_indep = 3)
 
-        ArcaneAim = core.InformedCharacterModifier("아케인 에임",pdamage = 40 + ceil(passive_level / 2))
-        VengenceOfAngelOn = core.InformedCharacterModifier("벤전스 오브 엔젤(on)", att = 50, pdamage_indep = 30, armor_ignore = 20, pdamage=-40, prop_ignore=10)
-        AngelRayArmorIgnore = core.InformedCharacterModifier("엔젤레이(방깎)", armor_ignore = (10 + ceil(self.combat / 3)) * 4)
+        ArcaneAim = core.InformedCharacterModifier(BishopSkills.ArcaneAim.value,pdamage = 40 + ceil(passive_level / 2))
+        VengenceOfAngelOn = core.InformedCharacterModifier(f"{BishopSkills.RighteouslyIndignant.value}(on)", att = 50, pdamage_indep = 30, armor_ignore = 20, pdamage=-40, prop_ignore=10)
+        AngelRayArmorIgnore = core.InformedCharacterModifier(f"{BishopSkills.AngelRay.value}(IED | 방깎)", armor_ignore = (10 + ceil(self.combat / 3)) * 4)
         return [WeaponConstant, Mastery, ArcaneAim, VengenceOfAngelOn, BlessingEnsemble, AngelRayArmorIgnore]
         
     def generate(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
@@ -159,44 +159,44 @@ class JobGenerator(ck.JobGenerator):
         PEACEMAKER_HIT = 3
 
         #Buff skills
-        Booster = core.BuffSkill("부스터", 0, 240000, rem = True).wrap(core.BuffSkillWrapper)
-        AdvancedBless = core.BuffSkill("어드밴스드 블레스", 0, 240000, att = 30 + self.combat*1 + 20, boss_pdamage = 10, rem = True).wrap(core.BuffSkillWrapper)
-        Heal = core.BuffSkill("힐", 600, 2000, cooltime=4000, pdamage_indep=10, red=True).wrap(core.BuffSkillWrapper)
+        Booster = core.BuffSkill(BishopSkills.MagicBooster.value, 0, 240000, rem = True).wrap(core.BuffSkillWrapper)
+        AdvancedBless = core.BuffSkill(BishopSkills.AdvancedBlessing.value, 0, 240000, att = 30 + self.combat*1 + 20, boss_pdamage = 10, rem = True).wrap(core.BuffSkillWrapper)
+        Heal = core.BuffSkill(BishopSkills.Heal.value, 600, 2000, cooltime=4000, pdamage_indep=10, red=True).wrap(core.BuffSkillWrapper)
         Infinity = adventurer.InfinityWrapper(self.combat)
-        EpicAdventure = core.BuffSkill("에픽 어드벤처", 0, 60*1000, cooltime = 120 * 1000, pdamage = 10).wrap(core.BuffSkillWrapper)
+        EpicAdventure = core.BuffSkill(BishopSkills.EpicAdventure.value, 0, 60*1000, cooltime = 120 * 1000, pdamage = 10).wrap(core.BuffSkillWrapper)
         
         Pray = PrayWrapper(vEhc, 2, 2)
         
         #Damage Skills
-        AngelRay = core.DamageSkill("엔젤레이", 630, 225 + 5*self.combat, 14).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)  # Vengeance use assumptions. 벤전스 사용 가정.
+        AngelRay = core.DamageSkill(BishopSkills.AngelRay.value, 630, 225 + 5*self.combat, 14).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)  # Vengeance use assumptions. 벤전스 사용 가정.
         
-        HeavensDoor = core.DamageSkill("헤븐즈도어", 270, 1000, 8, cooltime = 180 * 1000).wrap(core.DamageSkillWrapper)
+        HeavensDoor = core.DamageSkill(BishopSkills.HeavensDoor.value, 270, 1000, 8, cooltime = 180 * 1000).wrap(core.DamageSkillWrapper)
 
-        PeaceMakerInit = core.DamageSkill("피스메이커(시전)", 750, 0, 0, cooltime = 10 * 1000, red = True).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
-        PeaceMaker = core.DamageSkill("피스메이커", 0, 350 + 14*vEhc.getV(0,0), 4, cooltime = -1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
-        PeaceMakerFinal = core.DamageSkill("피스메이커(폭발)", 0, 350 + 14*vEhc.getV(0,0), 12, cooltime = -1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
-        PeaceMakerFinalBuff = core.BuffSkill("피스메이커(버프)", 0, (8 + SERVERLAG)*1000, pdamage = (5 + vEhc.getV(0,0) // 5) + (12 - PEACEMAKER_HIT), cooltime = -1).isV(vEhc,0,0).wrap(core.BuffSkillWrapper)
+        PeaceMakerInit = core.DamageSkill(f"{BishopSkills.Peacemaker.value}(cast | 시전)", 750, 0, 0, cooltime = 10 * 1000, red = True).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
+        PeaceMaker = core.DamageSkill(BishopSkills.Peacemaker.value, 0, 350 + 14*vEhc.getV(0,0), 4, cooltime = -1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
+        PeaceMakerFinal = core.DamageSkill(f"{BishopSkills.Peacemaker.value}(Explosion | 폭발)", 0, 350 + 14*vEhc.getV(0,0), 12, cooltime = -1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
+        PeaceMakerFinalBuff = core.BuffSkill(f"{BishopSkills.Peacemaker.value}(buff | 버프)", 0, (8 + SERVERLAG)*1000, pdamage = (5 + vEhc.getV(0,0) // 5) + (12 - PEACEMAKER_HIT), cooltime = -1).isV(vEhc,0,0).wrap(core.BuffSkillWrapper)
 
-        DivinePunishmentInit = core.DamageSkill("디바인 퍼니시먼트(개시)", 240, 0, 0, cooltime=85000).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
-        DivinePunishmentTick = core.DamageSkill("디바인 퍼니시먼트(키다운)", 240, 175+7*vEhc.getV(0,0), 5+5, cooltime=-1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
+        DivinePunishmentInit = core.DamageSkill(f"{BishopSkills.DivinePunishment.value}(cast | 개시)", 240, 0, 0, cooltime=85000).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
+        DivinePunishmentTick = core.DamageSkill(f"{BishopSkills.DivinePunishment.value}(tick | 키다운)", 240, 175+7*vEhc.getV(0,0), 5+5, cooltime=-1).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
     
         #Summoning skill
-        Bahamutt = core.SummonSkill("바하뮤트", 0, 3030, 170+2*self.combat, 3, 90 * 1000, cooltime = 120 * 1000, rem = True).setV(vEhc, 1, 2, False).wrap(core.SummonSkillWrapper)    # Delay 0 as it is automatically summoned at the end of the final Dem 25% stack and Libra ends. 최종뎀25%스택, 리브라 종료시 자동소환 되므로 딜레이 0.
-        AngelOfLibra = core.SummonSkill("엔젤 오브 리브라", 540, 4020, 500 + 20*vEhc.getV(3,1), 12, 30 * 1000, cooltime = 120 * 1000, red=True).isV(vEhc,3,1).wrap(core.SummonSkillWrapper)    # 50% final damage stack. 최종뎀50%스택.
+        Bahamutt = core.SummonSkill(BishopSkills.Bahamut.value, 0, 3030, 170+2*self.combat, 3, 90 * 1000, cooltime = 120 * 1000, rem = True).setV(vEhc, 1, 2, False).wrap(core.SummonSkillWrapper)    # Delay 0 as it is automatically summoned at the end of the final Dem 25% stack and Libra ends. 최종뎀25%스택, 리브라 종료시 자동소환 되므로 딜레이 0.
+        AngelOfLibra = core.SummonSkill(BishopSkills.AngelofBalance.value, 540, 4020, 500 + 20*vEhc.getV(3,1), 12, 30 * 1000, cooltime = 120 * 1000, red=True).isV(vEhc,3,1).wrap(core.SummonSkillWrapper)    # 50% final damage stack. 최종뎀50%스택.
         MirrorBreak, MirrorSpider = globalSkill.SpiderInMirrorBuilder(vEhc, 0, 0)
 
         #Unstable Memorize skills
-        EnergyBolt = core.DamageSkill("에너지 볼트", 660, 309, 1).wrap(core.DamageSkillWrapper)
-        HolyArrow = core.DamageSkill("홀리 애로우", 660, 518, 1).wrap(core.DamageSkillWrapper)
-        ShiningRay = core.DamageSkill("샤이닝 레이", 690, 254, 4).wrap(core.DamageSkillWrapper)
-        HolyFountain = core.DamageSkill("홀리 파운틴", 960, 0, 0).wrap(core.DamageSkillWrapper)
-        Dispell = core.DamageSkill("디스펠", 900, 0, 0).wrap(core.DamageSkillWrapper)
-        DivineProtection = core.DamageSkill("디바인 프로텍션", 870, 0, 0).wrap(core.DamageSkillWrapper)
-        Genesis = core.DamageSkill("제네시스", 630, 820, 6, cooltime=45000, red=True).wrap(core.DamageSkillWrapper)
-        BigBang = core.DamageSkill("빅뱅", 630, 480+6*self.combat, 4).wrap(core.DamageSkillWrapper)
-        Resurrection = core.DamageSkill("리저렉션", 900, 0, 0).wrap(core.DamageSkillWrapper)
+        EnergyBolt = core.DamageSkill(BishopSkills.EnergyBolt.value, 660, 309, 1).wrap(core.DamageSkillWrapper)
+        HolyArrow = core.DamageSkill(BishopSkills.HolyArrow.value, 660, 518, 1).wrap(core.DamageSkillWrapper)
+        ShiningRay = core.DamageSkill(BishopSkills.ShiningRay.value, 690, 254, 4).wrap(core.DamageSkillWrapper)
+        HolyFountain = core.DamageSkill(BishopSkills.HolyFountain.value, 960, 0, 0).wrap(core.DamageSkillWrapper)
+        Dispell = core.DamageSkill(BishopSkills.Dispel.value, 900, 0, 0).wrap(core.DamageSkillWrapper)
+        DivineProtection = core.DamageSkill(BishopSkills.DivineProtection.value, 870, 0, 0).wrap(core.DamageSkillWrapper)
+        Genesis = core.DamageSkill(BishopSkills.Genesis.value, 630, 820, 6, cooltime=45000, red=True).wrap(core.DamageSkillWrapper)
+        BigBang = core.DamageSkill(BishopSkills.BigBang.value, 630, 480+6*self.combat, 4).wrap(core.DamageSkillWrapper)
+        Resurrection = core.DamageSkill(BishopSkills.Resurrection.value, 900, 0, 0).wrap(core.DamageSkillWrapper)
 
-        VengenceOfAngel_Delay = core.DamageSkill("벤전스 오브 엔젤(딜레이)", 480, 0, 0).wrap(core.DamageSkillWrapper)
+        VengenceOfAngel_Delay = core.DamageSkill(f"{BishopSkills.RighteouslyIndignant.value}(delay | 딜레이)", 480, 0, 0).wrap(core.DamageSkillWrapper)
         
         ######   Wrappers    ######
         #Unstable Memorize
@@ -209,12 +209,12 @@ class JobGenerator(ck.JobGenerator):
             UnstableMemorize.add_skill(sk, weight)
             
         # Sacred Mark Control
-        SacredMark = core.StackSkillWrapper(core.BuffSkill("소환수 표식", 0, 999999 * 1000), 50)
-        Bahamutt.onTick(SacredMark.stackController(25, name="표식(25%)", dtype="set"))
-        AngelOfLibra.onTick(SacredMark.stackController(50, name="표식(50%)", dtype="set"))
+        SacredMark = core.StackSkillWrapper(core.BuffSkill("Sacred Mark | 소환수 표식", 0, 999999 * 1000), 50)
+        Bahamutt.onTick(SacredMark.stackController(25, name="Mark | 표식(25%)", dtype="set"))
+        AngelOfLibra.onTick(SacredMark.stackController(50, name="Mark | 표식(50%)", dtype="set"))
 
         for sk in [HolyArrow, ShiningRay, Genesis, BigBang, AngelRay, PeaceMaker, PeaceMakerFinal, DivinePunishmentTick]:
-            sk.onJustAfter(SacredMark.stackController(0, name="표식(소모)", dtype="set"))
+            sk.onJustAfter(SacredMark.stackController(0, name="Mark | 표식(Consumption | 소모)", dtype="set"))
             sk.add_runtime_modifier(SacredMark, lambda skill: core.CharacterModifier(pdamage_indep = skill.stack))
         
         # Peace Maker
@@ -225,7 +225,7 @@ class JobGenerator(ck.JobGenerator):
         
         # Libra - Bahamutt exclusive
         AngelOfLibra.onAfter(Bahamutt.controller(1))
-        Bahamutt.onConstraint(core.ConstraintElement("리브라와 동시사용 불가", AngelOfLibra, AngelOfLibra.is_not_active))
+        Bahamutt.onConstraint(core.ConstraintElement("Cannot be used with Libra | 리브라와 동시사용 불가", AngelOfLibra, AngelOfLibra.is_not_active))
 
         # Divine Punishment
         DivinePunishmentInit.onAfter(core.RepeatElement(DivinePunishmentTick, 33))
