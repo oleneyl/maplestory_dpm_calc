725a3549f4033d63cb7ac86d5f4b421bac1f1553
MrReds1324 <ryanb27@gmail.com>
2021-02-07 00:38:48 -0500

    Replaced skill name usage with enum for dual blade

--- dpmModule/jobs/dualblade.py
+++ dpmModule/jobs/dualblade.py
@@ -84,12 +84,12 @@ class JobGenerator(ck.JobGenerator):
     def get_passive_skill_list(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
         
-        Karma = core.InformedCharacterModifier("카르마", att = 30)
-        PhisicalTraining = core.InformedCharacterModifier("피지컬 트레이닝", stat_main = 30, stat_sub = 30)
+        Karma = core.InformedCharacterModifier(DualBladeSkills.ChannelKarma.value, att = 30)
+        PhisicalTraining = core.InformedCharacterModifier(DualBladeSkills.PhysicalTraining.value, stat_main = 30, stat_sub = 30)
         
-        SornsEffect = core.InformedCharacterModifier("쏜즈 이펙트", att = 30 + passive_level)
-        DualBladeExpert = core.InformedCharacterModifier("이도류 엑스퍼트", att = 30 + passive_level, pdamage_indep = 20 + passive_level // 2)
-        Sharpness = core.InformedCharacterModifier("샤프니스", crit = 35 + 3 * passive_level, crit_damage = 13 + passive_level)
+        SornsEffect = core.InformedCharacterModifier(DualBladeSkills.Thorns.value, att = 30 + passive_level)
+        DualBladeExpert = core.InformedCharacterModifier(DualBladeSkills.KataraExpert.value, att = 30 + passive_level, pdamage_indep = 20 + passive_level // 2)
+        Sharpness = core.InformedCharacterModifier(DualBladeSkills.Sharpness.value, crit = 35 + 3 * passive_level, crit_damage = 13 + passive_level)
         ReadyToDiePassive = thieves.ReadyToDiePassiveWrapper(vEhc, 2, 2)
         
         return [Karma, PhisicalTraining, SornsEffect, DualBladeExpert, Sharpness, ReadyToDiePassive]
@@ -111,12 +111,12 @@ class JobGenerator(ck.JobGenerator):
             return False
 
         ruleset = RuleSet()
-        ruleset.add_rule(ConditionRule('써든레이드', '파이널 컷', check_final_cut_time), RuleSet.BASE)
-        ruleset.add_rule(ConditionRule('아수라', '카르마 퓨리', lambda sk: sk.is_cooltime_left(6000, 1)), RuleSet.BASE)  # TODO: Optimized for 4 seconds of cooldown reduction, you need to receive and process cooldown reduction values. 쿨감 4초기준 최적화, 쿨감 수치를 받아와서 처리해야 함.
-        ruleset.add_rule(ComplexConditionRule('레디 투 다이', ['블레이드 스톰', '얼티밋 다크 사이트'], sync_burst_buff), RuleSet.BASE)
-        ruleset.add_rule(ComplexConditionRule('소울 컨트랙트', ['블레이드 스톰', '얼티밋 다크 사이트'], sync_burst_buff), RuleSet.BASE)
-        ruleset.add_rule(ConditionRule('블레이드 스톰', '얼티밋 다크 사이트', lambda sk: sk.is_active() or sk.is_cooltime_left(80000, 1)), RuleSet.BASE)
-        ruleset.add_rule(ConditionRule('메이플월드 여신의 축복', '얼티밋 다크 사이트', lambda sk: sk.is_active() or sk.is_usable()), RuleSet.BASE)
+        ruleset.add_rule(ConditionRule(DualBladeSkills.SuddenRaid.value, DualBladeSkills.FinalCut.value, check_final_cut_time), RuleSet.BASE)
+        ruleset.add_rule(ConditionRule(DualBladeSkills.AsurasAnger.value, DualBladeSkills.BladesofDestiny.value, lambda sk: sk.is_cooltime_left(6000, 1)), RuleSet.BASE)  # TODO: Optimized for 4 seconds of cooldown reduction, you need to receive and process cooldown reduction values. 쿨감 4초기준 최적화, 쿨감 수치를 받아와서 처리해야 함.
+        ruleset.add_rule(ComplexConditionRule(ThiefSkills.LastResort.value, [DualBladeSkills.BladeTempest.value, ThiefSkills.ShadowWalker.value], sync_burst_buff), RuleSet.BASE)
+        ruleset.add_rule(ComplexConditionRule(GlobalSkills.TermsAndConditions.value, [DualBladeSkills.BladeTempest.value, ThiefSkills.ShadowWalker.value], sync_burst_buff), RuleSet.BASE)
+        ruleset.add_rule(ConditionRule(DualBladeSkills.BladeTempest.value, ThiefSkills.ShadowWalker.value, lambda sk: sk.is_active() or sk.is_cooltime_left(80000, 1)), RuleSet.BASE)
+        ruleset.add_rule(ConditionRule(GlobalSkills.MapleWorldGoddessBlessing.value, ThiefSkills.ShadowWalker.value, lambda sk: sk.is_active() or sk.is_usable()), RuleSet.BASE)
         return ruleset
 
     def generate(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
@@ -139,44 +139,44 @@ class JobGenerator(ck.JobGenerator):
         '''
         passive_level = chtr.get_base_modifier().passive_level + self.combat
         #Buff skills
-        Booster = core.BuffSkill("부스터", 0, 180000, rem = True).wrap(core.BuffSkillWrapper)
+        Booster = core.BuffSkill(DualBladeSkills.KataraBooster.value, 0, 180000, rem = True).wrap(core.BuffSkillWrapper)
         
-        DarkSight = core.BuffSkill("다크 사이트", 0, 1, cooltime = -1).wrap(core.BuffSkillWrapper)#, pdamage_indep = 20 + 10 + int(0.2*vEhc.getV(3,3))).wrap(core.BuffSkillWrapper)
+        DarkSight = core.BuffSkill(DualBladeSkills.DarkSight.value, 0, 1, cooltime = -1).wrap(core.BuffSkillWrapper)#, pdamage_indep = 20 + 10 + int(0.2*vEhc.getV(3,3))).wrap(core.BuffSkillWrapper)
         
-        PhantomBlow = core.DamageSkill("팬텀 블로우", 540, 315 + 3 * self.combat, 6+1, modifier = core.CharacterModifier(armor_ignore = 44, pdamage = 20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
-        SuddenRaid = core.DamageSkill("써든레이드", 690, 494+5*self.combat, 7, cooltime = (30-2*(self.combat//2))*1000, red=True).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)    # Pacutt's remaining cooldown is reduced by 20%. 파컷의 남은 쿨타임 20% 감소.
-        SuddenRaidDOT = core.DotSkill("써든레이드(도트)", 0, 1000, 210 + 4 * self.combat, 1, 10000, cooltime = -1).wrap(core.DotSkillWrapper)
+        PhantomBlow = core.DamageSkill(DualBladeSkills.PhantomBlow.value, 540, 315 + 3 * self.combat, 6+1, modifier = core.CharacterModifier(armor_ignore = 44, pdamage = 20)).setV(vEhc, 0, 2, False).wrap(core.DamageSkillWrapper)
+        SuddenRaid = core.DamageSkill(DualBladeSkills.SuddenRaid.value, 690, 494+5*self.combat, 7, cooltime = (30-2*(self.combat//2))*1000, red=True).setV(vEhc, 2, 2, False).wrap(core.DamageSkillWrapper)    # Pacutt's remaining cooldown is reduced by 20%. 파컷의 남은 쿨타임 20% 감소.
+        SuddenRaidDOT = core.DotSkill(f"{DualBladeSkills.SuddenRaid.value}(DoT | 도트)", 0, 1000, 210 + 4 * self.combat, 1, 10000, cooltime = -1).wrap(core.DotSkillWrapper)
         
-        FinalCut = core.DamageSkill("파이널 컷", 450, 2000 + 20 * self.combat, 1, cooltime = 90000, red=True).wrap(core.DamageSkillWrapper)
-        FinalCutBuff = core.BuffSkill("파이널 컷(버프)", 0, 60000, rem = True, cooltime = -1, pdamage_indep = 40 + self.combat).wrap(core.BuffSkillWrapper)
+        FinalCut = core.DamageSkill(DualBladeSkills.FinalCut.value, 450, 2000 + 20 * self.combat, 1, cooltime = 90000, red=True).wrap(core.DamageSkillWrapper)
+        FinalCutBuff = core.BuffSkill(f"{DualBladeSkills.FinalCut.value}(Buff | 버프)", 0, 60000, rem = True, cooltime = -1, pdamage_indep = 40 + self.combat).wrap(core.BuffSkillWrapper)
         
-        EpicAdventure = core.BuffSkill("에픽 어드벤처", 0, 60*1000, cooltime = 120 * 1000, pdamage = 10).wrap(core.BuffSkillWrapper)
+        EpicAdventure = core.BuffSkill(DualBladeSkills.EpicAdventure.value, 0, 60*1000, cooltime = 120 * 1000, pdamage = 10).wrap(core.BuffSkillWrapper)
         
-        FlashBang = core.DamageSkill("플래시 뱅", 390, 250, 1, cooltime = 60000, red=True).wrap(core.DamageSkillWrapper)  # Random delay. 임의 딜레이.
-        FlashBangDebuff = core.BuffSkill("플래시 뱅(디버프)", 0, 50000/2, cooltime = -1, pdamage = 10 * 0.9).wrap(core.BuffSkillWrapper)
-        Venom = core.DotSkill("페이탈 베놈", 0, 1000, 160+5*passive_level, 2+(10+passive_level)//6, 8000, cooltime = -1).wrap(core.DotSkillWrapper)  # Stacks 3 times. 3회 중첩.
+        FlashBang = core.DamageSkill(DualBladeSkills.Flashbang.value, 390, 250, 1, cooltime = 60000, red=True).wrap(core.DamageSkillWrapper)  # Random delay. 임의 딜레이.
+        FlashBangDebuff = core.BuffSkill(f"{DualBladeSkills.Flashbang.value}(Debuff | 디버프)", 0, 50000/2, cooltime = -1, pdamage = 10 * 0.9).wrap(core.BuffSkillWrapper)
+        Venom = core.DotSkill(DualBladeSkills.ToxicVenom.value, 0, 1000, 160+5*passive_level, 2+(10+passive_level)//6, 8000, cooltime = -1).wrap(core.DotSkillWrapper)  # Stacks 3 times. 3회 중첩.
 
-        HiddenBladeBuff = core.BuffSkill("히든 블레이드(버프)", 0, 60000, cooltime = 90000, pdamage = 10).wrap(core.BuffSkillWrapper)
-        HiddenBlade = core.DamageSkill("히든 블레이드", 0, 140, 1).setV(vEhc, 5, 2, True).wrap(core.DamageSkillWrapper)
+        HiddenBladeBuff = core.BuffSkill(f"{DualBladeSkills.BladeClone.value}(Buff | 버프)", 0, 60000, cooltime = 90000, pdamage = 10).wrap(core.BuffSkillWrapper)
+        HiddenBlade = core.DamageSkill(DualBladeSkills.BladeClone.value, 0, 140, 1).setV(vEhc, 5, 2, True).wrap(core.DamageSkillWrapper)
         
-        Asura = core.DamageSkill("아수라", 810, 0, 0, cooltime = 60000).wrap(core.DamageSkillWrapper)
-        AsuraTick = core.DamageSkill("아수라(틱)", 300, 420, 4, modifier =core.CharacterModifier(armor_ignore = 100)).setV(vEhc, 1, 2, False).wrap(core.DamageSkillWrapper)
-        AsuraEnd = core.DamageSkill("아수라(종료)", 360, 0, 0, cooltime = -1).wrap(core.DamageSkillWrapper)
+        Asura = core.DamageSkill(DualBladeSkills.AsurasAnger.value, 810, 0, 0, cooltime = 60000).wrap(core.DamageSkillWrapper)
+        AsuraTick = core.DamageSkill(f"{DualBladeSkills.AsurasAnger.value}(Tick | 틱)", 300, 420, 4, modifier =core.CharacterModifier(armor_ignore = 100)).setV(vEhc, 1, 2, False).wrap(core.DamageSkillWrapper)
+        AsuraEnd = core.DamageSkill(f"{DualBladeSkills.AsurasAnger.value}(Ending | 종료)", 360, 0, 0, cooltime = -1).wrap(core.DamageSkillWrapper)
         
         UltimateDarksight = thieves.UltimateDarkSightWrapper(vEhc, 3, 3, 20)
         ReadyToDie = thieves.ReadyToDieWrapper(vEhc,4,4)
         MirrorBreak, MirrorSpider = globalSkill.SpiderInMirrorBuilder(vEhc, 0, 0)
         
-        BladeStorm = core.DamageSkill("블레이드 스톰", 120, 580+23*vEhc.getV(0,0), 7, red = True, cooltime = 90000, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
-        BladeStormTick = core.DamageSkill("블레이드 스톰(틱)", 210, 350+10*vEhc.getV(0,0), 5, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)  #10000/210 타
-        BladeStormEnd = core.DamageSkill("블레이드 스톰(종료)", 120, 0, 0).wrap(core.DamageSkillWrapper)
+        BladeStorm = core.DamageSkill(DualBladeSkills.BladeTempest.value, 120, 580+23*vEhc.getV(0,0), 7, red = True, cooltime = 90000, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
+        BladeStormTick = core.DamageSkill(f"{DualBladeSkills.BladeTempest.value}(Tick | 틱)", 210, 350+10*vEhc.getV(0,0), 5, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)  #10000/210 타
+        BladeStormEnd = core.DamageSkill(f"{DualBladeSkills.BladeTempest.value}(Ending | 종료)", 120, 0, 0).wrap(core.DamageSkillWrapper)
         
-        KarmaFury = core.DamageSkill("카르마 퓨리", 750, 400+16*vEhc.getV(1,1), 7 * 5, red = True, cooltime = 10000, modifier = core.CharacterModifier(armor_ignore = 30)).isV(vEhc,1,1).wrap(core.DamageSkillWrapper)
-        BladeTornado = core.DamageSkill("블레이드 토네이도", 540, 600+24*vEhc.getV(2,2), 7, red = True, cooltime = 12000, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,2,2).wrap(core.DamageSkillWrapper)
-        BladeTornadoSummon = core.SummonSkill("블레이드 토네이도(소환)", 0, 3000/5, 400+16*vEhc.getV(2,2), 6, 3000-1, cooltime=-1, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,2,2).wrap(core.SummonSkillWrapper)
-        BladeTornadoSummonMirrorImaging = core.SummonSkill("블레이드 토네이도(소환)(미러이미징)", 0, 3000/5, (400+16*vEhc.getV(2,2)) * 0.7, 6, 3000-1, cooltime=-1, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,2,2).wrap(core.SummonSkillWrapper)
+        KarmaFury = core.DamageSkill(DualBladeSkills.BladesofDestiny.value, 750, 400+16*vEhc.getV(1,1), 7 * 5, red = True, cooltime = 10000, modifier = core.CharacterModifier(armor_ignore = 30)).isV(vEhc,1,1).wrap(core.DamageSkillWrapper)
+        BladeTornado = core.DamageSkill(DualBladeSkills.BladeTornado.value, 540, 600+24*vEhc.getV(2,2), 7, red = True, cooltime = 12000, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,2,2).wrap(core.DamageSkillWrapper)
+        BladeTornadoSummon = core.SummonSkill(f"{DualBladeSkills.BladeTornado.value}(Summon | 소환)", 0, 3000/5, 400+16*vEhc.getV(2,2), 6, 3000-1, cooltime=-1, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,2,2).wrap(core.SummonSkillWrapper)
+        BladeTornadoSummonMirrorImaging = core.SummonSkill(f"{DualBladeSkills.BladeTornado.value}(Summon | 소환)({DualBladeSkills.MirrorImage.value})", 0, 3000/5, (400+16*vEhc.getV(2,2)) * 0.7, 6, 3000-1, cooltime=-1, modifier = core.CharacterModifier(armor_ignore = 100)).isV(vEhc,2,2).wrap(core.SummonSkillWrapper)
 
-        HauntedEdge = core.DamageSkill("헌티드 엣지-나찰", 0, 200+8*vEhc.getV(0,0), 4*5, cooltime=14000, red=True, modifier=core.CharacterModifier(armor_ignore=30)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
+        HauntedEdge = core.DamageSkill(f"{DualBladeSkills.HauntedEdge.value}(Yaksa | 나찰)", 0, 200+8*vEhc.getV(0,0), 4*5, cooltime=14000, red=True, modifier=core.CharacterModifier(armor_ignore=30)).isV(vEhc,0,0).wrap(core.DamageSkillWrapper)
         
         ######   Skill Wrapper   ######
     
@@ -205,12 +205,12 @@ class JobGenerator(ck.JobGenerator):
 
         SuddenRaid.onAfter(FinalCut.controller(0.2, "reduce_cooltime_p"))
 
-        PhantomBlow.onAfter(core.OptionalElement(HauntedEdge.is_available, HauntedEdge, name="헌티드 엣지 쿨타임 체크"))
+        PhantomBlow.onAfter(core.OptionalElement(HauntedEdge.is_available, HauntedEdge, name="Haunted Edge cooldown check | 헌티드 엣지 쿨타임 체크"))
         HauntedEdge.protect_from_running()
 
         for sk in [PhantomBlow, SuddenRaid, FinalCut, FlashBang, AsuraTick, 
             BladeStorm, BladeStormTick, KarmaFury, BladeTornado, HiddenBlade, HauntedEdge]:
-            jobutils.create_auxilary_attack(sk, 0.7, nametag='(미러이미징)')
+            jobutils.create_auxilary_attack(sk, 0.7, nametag=f'({DualBladeSkills.MirrorImage.value})')
         
         return(PhantomBlow,
                 [globalSkill.maple_heros(chtr.level, combat_level=self.combat), globalSkill.useful_sharp_eyes(), globalSkill.useful_combat_orders(),
