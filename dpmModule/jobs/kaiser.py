b7f84f4ba6456040685fdcceea5dc85dad09ccd8
ha <typ0@naver.com>
2021-02-19 06:05:10 +0900

    feat(kaiser): kmst 1.2.117
    * [윌 오브 소드] : 검이 날아갈 때의 액션 딜레이가 파이널 피규레이션 중에는 기존 대비 60% 감소되어 일반 상태와 동일한 딜레이로 적용됩니다.
    * [윌 오브 소드 : 스트라이크] : 검이 날아갈 때의 액션 딜레이가 파이널 피규레이션 중에는 기존 대비 60% 감소되어 일반 상태와 동일한 딜레이로 적용됩니다.

--- dpmModule/jobs/kaiser.py
+++ dpmModule/jobs/kaiser.py
@@ -61,12 +61,6 @@ class WillOfSwordSummonWrapper(core.BuffSkillWrapper):
         super(WillOfSwordSummonWrapper, self).__init__(skill)
         self.final_figuration = final_figuration
 
-    def get_delay(self):
-        if self.final_figuration.is_active():
-            return 600
-        else:
-            return 150
-
     def _off(self):
         self.timeLeft = 0
         return self._result_object_cache
@@ -101,12 +95,6 @@ class WillOfSwordStrikeWrapper(core.DamageSkillWrapper):
     def __init__(self, skill, final_figuration):
         super(WillOfSwordStrikeWrapper, self).__init__(skill)
         self.final_figuration = final_figuration
-
-    def get_delay(self):
-        if self.final_figuration.is_active():
-            return 600
-        else:
-            return 150
 
     def get_hit(self):
         if self.final_figuration.is_active():
@@ -221,7 +209,7 @@ class JobGenerator(ck.JobGenerator):
         
         GigaSlasher = GigaSlasherWrapper(core.DamageSkill("기가 슬래셔", 540, 330 + 2*self.combat, 9+1, modifier = core.CharacterModifier(pdamage = 20)).setV(vEhc, 0, 2, False), FinalFiguration)
     
-        AdvancedWillOfSword_Summon = WillOfSwordSummonWrapper(core.BuffSkill("어드밴스드 윌 오브 소드(소환)", 0, 9999999, cooltime = 10000, red=True), FinalFiguration)
+        AdvancedWillOfSword_Summon = WillOfSwordSummonWrapper(core.BuffSkill("어드밴스드 윌 오브 소드(소환)", 150, 9999999, cooltime = 10000, red=True), FinalFiguration)
         AdvancedWillOfSword = WillOfSwordWrapper(core.DamageSkill("어드밴스드 윌 오브 소드", 0, 400+3*passive_level, 4*5).setV(vEhc, 3, 2, True), FinalFiguration)
 
         InfernalBreath = core.DamageSkill("인퍼널 브레스", 780, 300 + 4*self.combat, 8, cooltime = (20-self.combat)*1000, red=True).setV(vEhc, 4, 2, True).wrap(core.DamageSkillWrapper)
