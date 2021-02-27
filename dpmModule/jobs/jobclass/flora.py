4388e262b979a18decbbe569b8d508208b545d1b
MrReds1324 <ryanb27@gmail.com>
2021-02-27 17:36:45 -0500

    Updated flora.py to only have korean with gettext translation enabled

--- dpmModule/jobs/jobclass/flora.py
+++ dpmModule/jobs/jobclass/flora.py
@@ -1,21 +1,22 @@
-from enum import Enum
-
 from ...kernel import core
 from ...kernel.core import VSkillModifier as V
 from ...kernel.core import CharacterModifier as MDF
 from ...character import characterKernel as ck
 from functools import partial
 
+import gettext
+_ = gettext.gettext
 
-class FloraSkills(Enum):
-    GrandisGoddessBlessing = 'Grandis Goddess\'s Blessing | 그란디스 여신의 축복'  # Taken from https://maplestory.fandom.com/wiki/Grandis_Goddess%27s_Blessing
-    ConversionOverdrive = 'Conversion Overdrive | 매직 서킷 풀드라이브'  # Taken from https://maplestory.fandom.com/wiki/Conversion_Overdrive
+
+class FloraSkills:
+    GrandisGoddessBlessing = _("그란디스 여신의 축복")  # "Grandis Goddess's Blessing" Taken from https://maplestory.fandom.com/wiki/Grandis_Goddess%27s_Blessing
+    ConversionOverdrive = _("매직 서킷 풀드라이브")  # "Conversion Overdrive" Taken from https://maplestory.fandom.com/wiki/Conversion_Overdrive
 
 
 # Lef. 레프.
 def FloraGoddessBlessWrapper(vEhc, num1, num2, WEAPON_ATT):
     # Equipment proportional increase is assumed to be the maximum. 장비 비례 증가 수치는 최대치로 가정.
-    FloraGoddessBless = core.BuffSkill(f"{FloraSkills.GrandisGoddessBlessing.value}(Lef | 레프)", 480, 40*1000, att = 10 + 3 * vEhc.getV(num1, num2) + 1.5 * WEAPON_ATT, cooltime = 240*1000, red=True).isV(vEhc, num1, num2).wrap(core.BuffSkillWrapper)
+    FloraGoddessBless = core.BuffSkill(_("{}(레프)").format(FloraSkills.GrandisGoddessBlessing), 480, 40*1000, att = 10 + 3 * vEhc.getV(num1, num2) + 1.5 * WEAPON_ATT, cooltime = 240*1000, red=True).isV(vEhc, num1, num2).wrap(core.BuffSkillWrapper)
     return FloraGoddessBless
 
 
@@ -26,8 +27,8 @@ def FloraGoddessBlessWrapper(vEhc, num1, num2, WEAPON_ATT):
 class MagicCircuitFullDriveBuilder():
     def __init__(self, vEhc, num1, num2, mana = 100):
         self.MANA = mana
-        self.MagicCircuitFullDriveBuff = core.BuffSkill(f"{FloraSkills.ConversionOverdrive.value}(Buff | 버프)", 540, (30+vEhc.getV(num1, num2))*1000, cooltime=200*1000, red=True, pdamage=(20+vEhc.getV(num1, num2)) * (self.MANA/100)).wrap(core.BuffSkillWrapper)
-        self.ManaStorm = core.DamageSkill(f"{FloraSkills.ConversionOverdrive.value}(Mana Storm | 마력 폭풍)", 0, 500+20*vEhc.getV(num1, num2), 3, cooltime = 4000).wrap(core.DamageSkillWrapper)
+        self.MagicCircuitFullDriveBuff = core.BuffSkill(_("{}(버프)").format(FloraSkills.ConversionOverdrive), 540, (30+vEhc.getV(num1, num2))*1000, cooltime=200*1000, red=True, pdamage=(20+vEhc.getV(num1, num2)) * (self.MANA/100)).wrap(core.BuffSkillWrapper)
+        self.ManaStorm = core.DamageSkill(_("{}(마력 폭풍)").format(FloraSkills.ConversionOverdrive), 0, 500+20*vEhc.getV(num1, num2), 3, cooltime = 4000).wrap(core.DamageSkillWrapper)
         self.UseManaStorm = core.OptionalElement(lambda: self.MagicCircuitFullDriveBuff.is_active() and self.ManaStorm.is_available(), self.ManaStorm)
         self.ManaStorm.protect_from_running()
         
