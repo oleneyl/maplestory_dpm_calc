81966dd41f951486acf8a5efe6c3082fde1e62c0
MrReds1324 <ryanb27@gmail.com>
2021-02-27 17:36:54 -0500

    Updated heroes.py to only have korean with gettext translation enabled

--- dpmModule/jobs/jobclass/heroes.py
+++ dpmModule/jobs/jobclass/heroes.py
@@ -1,19 +1,20 @@
-from enum import Enum
-
 from ...kernel import core
 from ...kernel.core import VSkillModifier as V
 from ...kernel.core import CharacterModifier as MDF
 from ...character import characterKernel as ck
 from functools import partial
 
+import gettext
+_ = gettext.gettext
 
-class HeroesSkills(Enum):
-    FreudsWisdom = 'Freud\'s Wisdom | 프리드의 가호'  # Taken from https://maplestory.fandom.com/wiki/Freud%27s_Wisdom
+
+class HeroesSkills:
+    FreudsWisdom = _("프리드의 가호")  # "Freud's Wisdom" Taken from https://maplestory.fandom.com/wiki/Freud%27s_Wisdom
 
 
 class FridWrapper(core.BuffSkillWrapper):
     def __init__(self, vEhc, num1, num2):
-        super(FridWrapper, self).__init__(core.BuffSkill(f"{HeroesSkills.FreudsWisdom.value}(?? | 더미)", 810, 30*1000, red=True).isV(vEhc, num1, num2))
+        super(FridWrapper, self).__init__(core.BuffSkill(_("{}(더미)").format(HeroesSkills.FreudsWisdom), 810, 30*1000, red=True).isV(vEhc, num1, num2))
         vlevel = vEhc.getV(num1, num2)
         self.modifierList = [
             core.CharacterModifier(),
