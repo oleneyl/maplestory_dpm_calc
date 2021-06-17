bb4c64c1734d42d46380ac8c752ac88c075a0d3d
ha <typ0@naver.com>
2021-06-17 13:18:29 +0900

    fix(adele): weapon constant

--- dpmModule/jobs/adele.py
+++ dpmModule/jobs/adele.py
@@ -124,7 +124,7 @@ class JobGenerator(ck.JobGenerator):
 
     def get_not_implied_skill_list(self, vEhc, chtr : ck.AbstractCharacter, options: Dict[str, Any]):
         passive_level = chtr.get_base_modifier().passive_level + self.combat
-        WeaponConstant = core.InformedCharacterModifier("무기상수",pdamage_indep = 34)
+        WeaponConstant = core.InformedCharacterModifier("무기상수",pdamage_indep = 30)
         Mastery = core.InformedCharacterModifier("숙련도", mastery=90+ceil(passive_level / 2))
 
         return [WeaponConstant, Mastery]
